617260af975ab13ac9aaeb27f9dc6708849f3918
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-01-18 20:12:36 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (6.24%) - LeetSync

--- 507-perfect-number/perfect-number.java
+++ 507-perfect-number/perfect-number.java
@@ -1,10 +1,11 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
-        int sum=0;
-        for(int i=1; i<num; i++){
-            if(num%i==0)
-                sum+=i;
-        }
-        return sum==num;
+        // int sum=0;
+        // for(int i=1; i<num; i++){
+        //     if(num%i==0)
+        //         sum+=i;
+        // }
+        // return sum==num;
+        return num == 6||num == 28||num == 496||num == 8128||num == 33550336; //One line
     }
 }
