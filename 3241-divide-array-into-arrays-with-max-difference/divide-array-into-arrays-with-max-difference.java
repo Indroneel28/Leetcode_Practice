bcb6200274faf3e0f66ff20dc788c990aa923349
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-02-01 12:00:01 +0530

    Time: 54 ms (5.10%) | Memory: 60.5 MB (6.73%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.java
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.java
@@ -6,15 +6,17 @@ class Solution {
         System.out.println(Arrays.toString(nums));
 
         int[][] store = new int[nums.length/3][3];
-        int x= 0;
-        for (int i=0; i<store.length; i++){
-            for (int j=0; j<store[0].length; j++){
-                store[i][j]= nums[x];
-                x++;
+        int m=0, n=0;
+        for (int i=0; i<nums.length; i++){
+            store[m][n]= nums[i];
+            if (n==2){
+                if (store[m][2]-store[m][0]>k)
+                    return new int[][]{};
+                m++;
+                n=0;
             }
-            
-            if (store[i][2]-store[i][0]>k)
-                return new int[][]{}; 
+            else
+                n++;
         }
         return store;
     }
