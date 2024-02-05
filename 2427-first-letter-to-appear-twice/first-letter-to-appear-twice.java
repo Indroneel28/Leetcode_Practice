3d6186f7b12162eecdb846d6eeae6363d897b5c7
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-02-05 11:37:26 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (72.34%) - LeetSync

--- 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.java
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.java
@@ -1,10 +1,21 @@
 class Solution {
     public char repeatedCharacter(String s) {
         //METHOD 1:- USING HASHMAP
+        /*
         HashMap<Character,Integer> hashMap= new HashMap<>();
         for (int i=0; i<s.length(); i++){
             hashMap.put(s.charAt(i),hashMap.getOrDefault(s.charAt(i),0)+1);
             if (hashMap.get(s.charAt(i))==2)
+                return s.charAt(i);
+        }
+        return 'A'; //This will never execute
+         */
+
+        //METHOD 2:- USING ARRAY
+        int[] arr= new int[26];
+        for (int i=0; i<s.length(); i++){
+            arr[s.charAt(i)-97]++;
+            if (arr[s.charAt(i)-97]==2)
                 return s.charAt(i);
         }
         return 'A'; //This will never execute
