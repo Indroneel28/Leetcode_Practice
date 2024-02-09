30bc5cfb0bb90897ee196fbe64815511403a8f15
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-02-09 23:35:55 +0530

    Time: 0 ms (100.00%) | Memory: 66.9 MB (76.67%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.java
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.java
@@ -5,7 +5,7 @@ class Solution {
         int a= edges[0][0], b= edges[0][1], c= edges[1][0], d= edges[1][1];
         if (a==c || a==d)
             return a;
-        if (b==c || b==d)
+        if (b==c || b==d) //This will definitely execute as it's confirm as given in constraint
             return b;
         
         return -1; //This will never execute
