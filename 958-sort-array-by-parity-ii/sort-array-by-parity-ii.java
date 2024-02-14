e563cd02112722f69c66faefe6e29aa9369fc67a
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-02-14 18:07:41 +0530

    Time: 4 ms (23.99%) | Memory: 47.7 MB (10.56%) - LeetSync

--- 958-sort-array-by-parity-ii/sort-array-by-parity-ii.java
+++ 958-sort-array-by-parity-ii/sort-array-by-parity-ii.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] sortArrayByParityII(int[] nums) {
         //METHOD 1:- USING EXTRA ARRAY
+        /*
         int[] store= new int[nums.length];
         int m=0, n=1;
         //Even indexes(0,2,4,...) should be even and odd indexes(1,3,5,...) should be odd
@@ -15,5 +16,27 @@ class Solution {
             }
         }
         return store;
+         */
+        //TC is O(n)
+
+        // Follow Up: Could you solve it in-place?
+        //Let's solve using inplace algorithm
+
+        //METHOD 2:- INPLACE ALGORITHM
+
+        int x= nums.length-1;
+        for (int i=0; i<nums.length; i++){
+            if ((i%2==0 && nums[i]%2==1) || (i%2==1 && nums[i]%2==0)){
+                    //Exchange number at index x and i
+                    int store= nums[i];
+                    nums[i]= nums[x];
+                    nums[x]= store;
+                    x--; i--; //I-- BECAUSE i-- + i++ = 0 means i is not incremented
+
+            }
+            else 
+                x= nums.length-1;
+        }
+        return nums;
     }
 }
