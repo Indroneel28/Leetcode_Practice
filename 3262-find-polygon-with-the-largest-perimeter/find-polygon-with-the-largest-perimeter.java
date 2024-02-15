773a8d6a060f3f18bd657edce6bc1b5568b384f4
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-02-15 11:25:48 +0530

    Time: 28 ms (98.63%) | Memory: 56 MB (92.93%) - LeetSync

--- 3262-find-polygon-with-the-largest-perimeter/find-polygon-with-the-largest-perimeter.java
+++ 3262-find-polygon-with-the-largest-perimeter/find-polygon-with-the-largest-perimeter.java
@@ -1,5 +1,7 @@
 class Solution {
     public long largestPerimeter(int[] nums) {
+        //USING GREEDY STRATEGY:- SORT IN INCREASING ORDER
+        /*
         Arrays.sort(nums);
         int n= nums.length-1;
         for (int i=n; i>=0; i--){
@@ -7,9 +9,23 @@ class Solution {
             for (int j= i-1; j>=0; j--){
                 sum+= nums[j];
             }
-            if (sum>nums[i])
+            if (sum > nums[i]) //strictly greater
                 return sum+nums[i];
         }
         return -1;
+         */
+        //TC:- O(n^2)
+
+        //USING GREEDY STRATEGY 2
+        Arrays.sort(nums);
+        long previousSum= 0;
+        long ans= -1;
+        for (int num: nums){
+            if (previousSum > num)
+                ans= previousSum+num;
+            previousSum+=num;
+        }
+        return ans;
+        //TC:- O(nlog) because sorting dominates
     }
 }
