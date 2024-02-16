886638a8bd70a76c25981214c50373dfe3922de0
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-02-16 20:25:44 +0530

    Time: 68 ms (24.97%) | Memory: 56.5 MB (88.99%) - LeetSync

--- 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.java
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.java
@@ -1,21 +1,37 @@
 class Solution {
     public int findLeastNumOfUniqueInts(int[] arr, int k) {
-       HashMap<Integer,Integer> hashMap= new HashMap<>();
+        // HashMap<Integer,Integer> hashMap= new HashMap<>();
+        // for (int element: arr){
+        //     hashMap.put(element,hashMap.getOrDefault(element,0)+1);
+        // }
+
+        // ArrayList<Integer> list= new ArrayList<>(hashMap.values());
+        // list.sort(null);
+
+        // int m= 0;
+        // for (int index= 0; index<list.size(); index++){
+        //     m+=list.get(index);
+        //     if (m==k)
+        //         return list.size()-index-1;
+        //     if (m>k)
+        //         return list.size()-index;
+        // }
+        // return -1;
+        HashMap<Integer,Integer> hashMap= new HashMap<>();
         for (int element: arr){
             hashMap.put(element,hashMap.getOrDefault(element,0)+1);
         }
 
-        ArrayList<Integer> list= new ArrayList<>(hashMap.values());
+        PriorityQueue<Integer> list= new PriorityQueue<>(hashMap.values());
         System.out.println(list);
-        list.sort(null);
 
         int m= 0;
-        for (int index= 0; index<list.size(); index++){
-            m+=list.get(index);
-            if (m==k)
-                return list.size()-index-1;
-            if (m>k)
-                return list.size()-index;
+        while (!list.isEmpty()){
+            m+=list.poll();
+            if(m==k)
+                return list.size();
+            if (m>=k)
+                return list.size()+1;
         }
         return -1;
     }
