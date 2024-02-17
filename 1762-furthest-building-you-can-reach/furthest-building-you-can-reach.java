fb76c181ffb7be3079df05c430276ce19b43519b
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-02-17 23:11:42 +0530

    Time: 21 ms (20.22%) | Memory: 57.4 MB (65.67%) - LeetSync

---
+++ 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int furthestBuilding(int[] heights, int bricks, int ladders) {
+        PriorityQueue<Integer> queue= new PriorityQueue<>();
+        int usedBricks= 0;
+        
+        for (int i=1; i<heights.length; i++){
+            int diff= heights[i]-heights[i-1];
+            if (diff>0)
+                queue.add(diff);
+            if (!queue.isEmpty() && queue.size()>ladders){
+                usedBricks+=queue.poll();
+                if (usedBricks>bricks)
+                    return i-1; //Coz we can't climb ith index from (i-1)th index
+            }
+        }
+        return heights.length-1;
+    }
+}
