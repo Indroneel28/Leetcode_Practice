caf33e043e763064545eec6844e033840649e714
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-02-17 23:12:52 +0530

    Time: 20 ms (43.55%) | Memory: 57.6 MB (40.57%) - LeetSync

--- 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.java
+++ 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.java
@@ -5,6 +5,8 @@ class Solution {
         
         for (int i=1; i<heights.length; i++){
             int diff= heights[i]-heights[i-1];
+            if (diff<0)
+                continue;
             if (diff>0)
                 queue.add(diff);
             if (!queue.isEmpty() && queue.size()>ladders){
