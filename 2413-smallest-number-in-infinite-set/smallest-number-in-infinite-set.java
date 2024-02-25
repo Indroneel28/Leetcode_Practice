22d92c05419b70f85f0c466e69fee89b0cfd24fb
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-02-25 11:07:49 +0530

    Time: 74 ms (6.87%) | Memory: 45.4 MB (44.33%) - LeetSync

--- 2413-smallest-number-in-infinite-set/smallest-number-in-infinite-set.java
+++ 2413-smallest-number-in-infinite-set/smallest-number-in-infinite-set.java
@@ -1,24 +1,18 @@
-class MyComparator implements Comparator<Integer>{
-    public int compare(Integer i, Integer j){
-        return i.compareTo(j);
-    }
-}
 class SmallestInfiniteSet{
-    ArrayList<Integer> list;
+    PriorityQueue<Integer> queue;
     public SmallestInfiniteSet() {
-        this.list= new ArrayList<Integer>();
+        this.queue= new PriorityQueue<>();
         for (int i=1; i<=1000; i++)
-            list.add(i);
+            queue.add(i);
     }
 
     public int popSmallest() {
-        return list.remove(0);
+        return queue.remove();
     }
 
     public void addBack(int num) {
-        if (!list.contains(num))
-            list.add(num);
-        list.sort(new MyComparator());
+        if (!queue.contains(num))
+            queue.add(num);
     }
 }
 
