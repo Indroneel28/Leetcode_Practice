a24ffe10aaa67d86732fe85672906dd21a13f0ae
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-02-26 07:01:56 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (54.87%) - LeetSync

---
+++ 100-same-tree/same-tree.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        //Method 1:- Recursive Approach
+        if (p==null && q==null)
+            return true;
+        if (p==null || q==null || p.val!=q.val)
+            return false;
+        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+    }
+
+
+    //Method 2:- Iterative Approach
+    /*
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(p);
+        queue.add(q);
+        while(!queue.isEmpty()){
+            TreeNode f = queue.poll();
+            TreeNode s = queue.poll();
+            if(f == null && s == null){
+                continue;
+            }else if(f == null || s == null || f.val != s.val){
+                return false;
+            }
+            queue.add(f.left);
+            queue.add(s.left);
+            queue.add(f.right);
+            queue.add(s.right);
+        }
+        return true;
+        */
+}
