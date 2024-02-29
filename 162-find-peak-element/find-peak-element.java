9caebff2be8a1127cb28404d00c7590eee53480c
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-02-29 21:23:38 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (84.76%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,12 +1,25 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        int index=0, element=nums[0];
-        for (int i=1; i<nums.length; i++){
-            if (nums[i]>element){
-                index= i;
-                element= nums[i];
-            }
+        // If the array contains multiple peaks, return the index to any of the peaks.
+        if (nums.length==1)
+            return 0;
+    
+        int i=0, j= nums.length-1;
+         while (i<=j){
+            int mid= (i+j)/2;
+            if (mid==0 && nums[mid]>nums[mid+1])
+                return mid;
+            else if (mid==nums.length-1 && nums[mid]>nums[mid-1])
+                return mid;
+            else if (nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1])
+                return mid;
+            
+            else if (nums[mid]<nums[mid+1])
+                i= mid+1;
+            else
+                j= mid-1;
         }
-        return index;
+        return -1; //will never execute as for constraints
+
     }
 }
