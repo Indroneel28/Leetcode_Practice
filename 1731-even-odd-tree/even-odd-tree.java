4f8f4947c214a25cdde560385d3123d91f12baba
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-02-29 22:57:11 +0530

    Time: 13 ms (50.59%) | Memory: 67 MB (20.51%) - LeetSync

---
+++ 1731-even-odd-tree/even-odd-tree.java
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isEvenOddTree(TreeNode root) {
+        //USING BFS IN GRAPH
+        Queue<TreeNode> queue= new LinkedList<>();
+        queue.add(root);
+        int level= 0;
+        while (!queue.isEmpty()){
+            int size= queue.size(), store;
+            boolean levelEven= level%2==0;
+            if (levelEven)
+                store= Integer.MIN_VALUE;
+            else
+                store= Integer.MAX_VALUE;
+            ArrayList<TreeNode> arrayList= new ArrayList<>();
+            for (int i=1; i<=size; i++){
+                TreeNode node= queue.remove();
+                if (node.left!=null){
+                    queue.add(node.left);
+                }
+                if (node.right!=null){
+                    queue.add(node.right);
+                }
+                if (levelEven){
+                    if (node.val%2==0)
+                        return false;
+                    if (store>=node.val){
+                        return false;
+                    }
+                    else if (node.val>store){
+                        store= node.val;
+                    }
+                }
+                else {
+                    if (node.val%2==1)
+                        return false;
+                    if (store<=node.val){
+                        return false;
+                    }
+                    else if (node.val<store){
+                        store= node.val;
+                    }
+                }
+            }
+            level++;
+        }
+        return true;
+    }
+}
