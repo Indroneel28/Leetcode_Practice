90128dd672223872d5a04584a4a8e5376a0c0f6d
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-05-01 10:20:22 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (99.95%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.java
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.java
@@ -1,5 +1,7 @@
 class Solution {
     public String reversePrefix(String word, char ch) {
+        //METHOD 1:- USING STRING
+        /*
         int index= word.indexOf(ch);
         String newStr="";
         for (int i=0; i<=index; i++){
@@ -7,5 +9,12 @@ class Solution {
         }
         newStr+=word.substring(index+1);
         return newStr;
+         */
+
+        //method 2:- using string builder
+        StringBuilder sb= new StringBuilder(word.substring(0,word.indexOf(ch)+1));
+        sb.reverse();
+        sb.append(word,word.indexOf(ch)+1,word.length());
+        return sb.toString();
     }
 }
