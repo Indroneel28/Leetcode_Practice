9a816e14ece2177f2d90b46de7f9a2c680bd66f8
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-05-22 00:39:18 +0530

    Time: 1 ms (57.66%) | Memory: 42.7 MB (41.65%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,10 +1,10 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        HashSet<List<Integer>> answer= new HashSet<>();
+        ArrayList<List<Integer>> answer= new ArrayList<>();
         traverse(answer,new ArrayList<>(),nums,0);
         return new ArrayList<>(answer);
     }
-    private void traverse(HashSet<List<Integer>> answer, List<Integer> list, int[] nums, int index){
+    private void traverse(ArrayList<List<Integer>> answer, List<Integer> list, int[] nums, int index){
         if (index==nums.length){
             answer.add(new ArrayList<>(list));
             return;
