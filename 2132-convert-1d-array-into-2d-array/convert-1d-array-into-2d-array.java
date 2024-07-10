2c26835f73df7562b2879246cc1f36952fb3dd3d
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-07-11 00:19:28 +0530

    Time: 4 ms (64.59%) | Memory: 55.3 MB (73.98%) - LeetSync

---
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        if (original.length!=m*n)
+            return new int[0][0];
+
+        int[][] matrix= new int[m][n]; // m is no. of rows and n is no. of columns
+        int x= 0, y= 0;
+        for (int i=0; i<original.length; i++){
+            matrix[x][y]= original[i];
+            y++;
+            if (y==n){
+                y=0;
+                x++;
+            }
+        }
+        return matrix;
+    }
+}
