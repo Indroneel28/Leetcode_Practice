751d60e84d0839f7482f24e130be23fc7097b713
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-07-12 00:06:47 +0530

    Time: 6 ms (28.25%) | Memory: 43.1 MB (17.81%) - LeetSync

---
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String reverseParentheses(String s) {
+        //USING STACK APPROACH
+        //Strings will be reversed automatically in Stack
+
+        Stack<Character> stack= new Stack<>();
+        for (int i=0; i<s.length(); i++){
+            char c= s.charAt(i);
+
+            //Push each and any type of character in the stack
+            stack.push(c);
+
+            //If character inserted is ')' then we will extract characters one by one
+            if (stack.peek()==')') {
+                StringBuilder sb= new StringBuilder();
+                stack.pop(); //Extract ')'
+                while (stack.peek() != '(') {
+                    sb.append(stack.pop()); //Extract one by one character from last and put inside sb
+                }
+                stack.pop(); //Extract '(' and kick it out
+                for (int j=0; j<sb.length(); j++){
+                    stack.push(sb.charAt(j));
+                }
+            }
+        }
+        String answer="";
+        while (!stack.isEmpty()){
+            answer=  stack.pop()+ answer;
+        }
+        return answer;
+    }
+}
