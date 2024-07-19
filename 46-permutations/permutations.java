504c2c5637bb99f78b52c689b432023363b6f510
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-07-19 21:33:50 +0530

    Time: 1 ms (93.71%) | Memory: 43.8 MB (98.31%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,4 +1,6 @@
 class Solution {
+    //METHOD 1:- USING check[] array
+    /*
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> answer= new ArrayList<>();
         List<Integer> p= new ArrayList<>();
@@ -7,6 +9,7 @@ class Solution {
         return answer;
     }
 
+    //Striver's Recursion L12
     private void permutation(List<List<Integer>> answer, List<Integer> p, int[] nums, boolean[] check){
         if (p.size()==nums.length){
             answer.add(new ArrayList<>(p));
@@ -24,5 +27,37 @@ class Solution {
                 check[i]= false;
             }
         }
+    }
+    */
+
+    //METHOD 2:- USING  index VARIABLE
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> answer= new ArrayList<>();
+        int index= 0;
+        permutation(answer,nums,index);
+        return answer;
+    }
+
+    //Striver's Recursion L13
+    private void permutation(List<List<Integer>> answer, int[] nums, int index){
+        if (index==nums.length) {
+            ArrayList<Integer> list= new ArrayList<>();
+            for (int num: nums)
+                list.add(num);
+            answer.add(new ArrayList<>(list));
+            return;
+        }
+        for (int i=index; i<nums.length; i++){
+            swap(nums,index,i);
+            permutation(answer,nums,index+1);
+            swap(nums,index,i);
+        }
+    }
+
+    private void swap(int[] nums, int i, int j){
+        int dup= nums[i];
+        nums[i]= nums[j];
+        nums[j]= dup;
     }
 }
