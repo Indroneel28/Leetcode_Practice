077a0ba8cb91e9ec02a1f6c735517a5efbc34350
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-07-22 10:14:09 +0530

    Time: 7 ms (90.64%) | Memory: 45.4 MB (32.21%) - LeetSync

--- 2502-sort-the-people/sort-the-people.java
+++ 2502-sort-the-people/sort-the-people.java
@@ -1,30 +1,19 @@
-class Solution {
+
+
+//METHOD 2:- BY USING HASHMAP
+public class Solution {
     public String[] sortPeople(String[] names, int[] heights) {
-        ArrayList<Name> list= new ArrayList<>();
-        for (int i=0; i<names.length; i++){
-            Name<String,Integer> name= new Name<>(names[i],heights[i]);
-            list.add(name);
+        int len= names.length;
+        HashMap<Integer,String> map= new HashMap<>();
+        for (int i=0; i<len; i++){
+            map.put(heights[i],names[i]);
         }
-        Collections.sort(list);
-        String[] answer= new String[names.length];
-        for (int i=0; i<names.length; i++){
-            answer[i]= (String) list.get(i).name;
+        Arrays.sort(heights);
+
+        String[] answer= new String[len];
+        for (int i=0; i<len; i++){
+            answer[i]= map.get(heights[len-1-i]);
         }
         return answer;
-    }
-}
-
-class Name<String,Integer> implements Comparable<Name<String,Integer>>{
-    String name;
-    int height;
-
-    public Name(String n, int h){
-        this.name= n;
-        this.height= h;
-    }
-
-    @Override
-    public int compareTo(Name o) {
-        return o.height-this.height;
     }
 }
