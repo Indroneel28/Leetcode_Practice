1af3b8e9f071ea61afe012b297842ada67aaec9b
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-07-24 13:59:29 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (72.11%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -12,7 +12,8 @@ class Solution {
         return list.size();
          */
 
-        //METHOD 2:- LOGIC
+        //METHOD 2:- USING TWO POINTERS APPROACH
+        /*
         int index= 1;
         for (int i=0; i<nums.length-1; i++){
             if (nums[i]!=nums[i+1]){
@@ -21,5 +22,29 @@ class Solution {
             }
         }
         return index;
+         */
+
+        //METHOD 3:- USING SET DATA STRUCTURE
+        /*
+        Set<Integer> set= new HashSet<>();
+        int i=0;
+        for (int num: nums) {
+            if (set.add(num)){
+                nums[i]= num;
+                i++;
+            }
+        }
+        return set.size();
+         */
+
+        //METHOD 4:- USING TWO POINTERS OPTIMIZED APPROACH
+        int i=0;
+        for (int j=1; j<nums.length; j++){
+            if (nums[i]!=nums[j]){
+                i++;
+                nums[i]= nums[j];
+            }
+        }
+        return i+1;
     }
 }
