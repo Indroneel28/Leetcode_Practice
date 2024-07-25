d7d48972242acd98e67870812a29f9268eff5b2b
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-07-25 16:30:10 +0530

    Time: 2 ms (83.04%) | Memory: 45.7 MB (78.27%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,11 +1,15 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        //Two Pointers with swapping in single iteration
         int m=0;
         for (int i=0; i<nums.length; i++){
-            if (nums[i]!=0)
-                nums[m++]= nums[i];
+            if (nums[i]!=0) {
+                //swap nums[m] && nums[i]
+                int temp= nums[m];
+                nums[m] = nums[i];
+                nums[i]= temp;
+                m++;
+            }
         }
-        for (int i=m; i<nums.length; i++)
-            nums[i]= 0;
     }
 }
