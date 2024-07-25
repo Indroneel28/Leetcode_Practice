ddd868338a473a1b461cc16a08631943a92e49b5
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-07-25 13:16:39 +0530

    Time: 27 ms (57.62%) | Memory: 56.6 MB (32.66%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,14 +1,60 @@
 class Solution {
     public int[] sortArray(int[] nums) {
         //METHOD 1:- Heap Sort (using Priority Queue)
-        //Tc is O(nlogn) and SC is O(1)
+        //TC is O(nlogn) and SC is O(1)
+        /*
         PriorityQueue<Integer> queue= new PriorityQueue<>();
         for (int num: nums)
             queue.add(num);
         for (int i=0; i<nums.length; i++)
             nums[i]= queue.poll();
         return nums;
+         */
+        //86 ms Beats 19.37%
+
+        //METHOD 2:- Merge Sort
+        //TC is O(nlogn) and SC is O(n)
+        mergesort(nums);
+        return nums;
     }
 
+    private void mergesort(int[] arr){
+        mergeSorting(arr,0,arr.length-1);
+    }
+    private void mergeSorting(int[] arr, int low, int high){
+        if (low>=high)
+            return;
+        int mid= (low+high)/2;
+        mergeSorting(arr,low,mid);
+        mergeSorting(arr,mid+1,high);
+        merge(arr,low,mid,high);
+    }
+    private void merge(int[] arr, int low, int mid, int high){
+        int[] temp= new int[high-low+1];
+        int k= 0;
+        int i= low, j= mid+1;
 
+        //Sorting using two pointers i and j
+        while (i<=mid && j<=high){
+            if (arr[i]<arr[j]){
+                temp[k++]= arr[i++];
+            }
+            else{
+                temp[k++]= arr[j++];
+            }
+        }
+        //Inserting remaining elements
+        while (i<=mid){
+            temp[k++]= arr[i++];
+        }
+        while(j<=high){
+            temp[k++]= arr[j++];
+        }
+
+        //Storing in arr from temp
+        int n=0;
+        for (int m=low; m<=high; m++){
+            arr[m]= temp[n++];
+        }
+    }
 }
