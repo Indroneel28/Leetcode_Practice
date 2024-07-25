b74ce0b8d91ee530dd3de60f95a90f3ee87e4873
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-07-25 15:59:19 +0530

    Time: 2 ms (83.04%) | Memory: 46 MB (56.25%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,36 +1,11 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        //USING TWO POINTERS METHOD
-
-        //COMPLEX METHOD
-        /*
-        int j=0;
-        for (int i=0; i<nums.length && j<nums.length; i++){
-            if (nums[i]==0){
-                if (nums[j]!=0){
-                    //swap
-                    int temp= nums[i];
-                    nums[i]= nums[j];
-                    nums[j]= temp;
-                    j=i; //restore j again
-                }
-                i--;
-            }
-            j++;
-        }
-        //BUT ONLY ONE FOR LOOP SO IT'S GOOD
-         */
-
-        //EASY METHOD
-        int c= 0;
+        int m=0;
         for (int i=0; i<nums.length; i++){
-            if (nums[i]!=0){
-                nums[c]= nums[i];
-                c++;
-            }
+            if (nums[i]!=0)
+                nums[m++]= nums[i];
         }
-        
-        for (int i=c; i<nums.length; i++)
+        for (int i=m; i<nums.length; i++)
             nums[i]= 0;
     }
 }
