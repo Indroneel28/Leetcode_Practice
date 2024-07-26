15ebac6f7537ed1334e206a5da3e1866c176c21b
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-07-26 23:52:53 +0530

    Time: 4 ms (37.78%) | Memory: 41.7 MB (16.36%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -1,5 +1,7 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
+        //METHOD 1:- USING EXTRA ARRAY
+        /*
         char[] store= new char[26];
         for (int i=0; i<sentence.length(); i++){
             char c= sentence.charAt(i);
@@ -12,5 +14,15 @@ class Solution {
                 return false;
         }
         return true;
+        //TC is O(n) and SC is O(26)
+         */
+
+        //METHOD 2:- USING HASHSET
+        HashSet<Character> set= new HashSet<>();
+        for (int i=0; i<sentence.length(); i++){
+            set.add(sentence.charAt(i));
+        }
+        return set.size()==26;
+        //Tc is O(n) and SC is O(n)
     }
 }
