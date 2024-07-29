ff5589be16a1fa84a989f5f20a5da53b37e3dda0
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-07-29 20:51:31 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (35.23%) - LeetSync

--- 2269-count-elements-with-strictly-smaller-and-greater-elements/count-elements-with-strictly-smaller-and-greater-elements.java
+++ 2269-count-elements-with-strictly-smaller-and-greater-elements/count-elements-with-strictly-smaller-and-greater-elements.java
@@ -1,19 +1,42 @@
 class Solution {
     public int countElements(int[] nums) {
+        //METHOD 1:- Brute Force Method
+        /*
         int count= 0;
         for (int i=0; i<nums.length; i++){
             boolean small= false, large= false;
+            //Finding smaller and larger elements than nums[i]
             for (int j=0; j<nums.length; j++){
-                if (nums[j]<nums[i])
+                if (nums[j]<nums[i]) //For smaller element
                     small= true;
-                else if (nums[j]>nums[i])
+                else if (nums[j]>nums[i]) //For larger element
                     large= true;
-                if (large && small){
+                if (large && small){ //if large and small element exists
                     count++;
                     break;
                 }
             }
         }
         return count;
+         */
+        //TC is O(N^2) and SC is O(1)
+
+        //METHOD 2:- USING BRAIN
+        //Let's find smallest and largest element of the array
+        int largest= nums[0], smallest= nums[0];
+        for (int num: nums){
+            if (num>largest)
+                largest= num;
+            else if (num<smallest)
+                smallest= num;
+        }
+        
+        int count= 0;
+        for (int num: nums){
+            if (num!=largest && num!=smallest)
+                count++;
+        }
+        return count;
+        //TC is O(2n) and Sc is O(1)
     }
 }
