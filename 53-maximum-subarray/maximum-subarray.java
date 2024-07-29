3df5f9a5e7a73968ae9e3fc3009e6db8d0124e00
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-07-29 16:45:53 +0530

    Time: 1 ms (99.61%) | Memory: 56.9 MB (66.14%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,13 +1,32 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int sum= 0, maxSum= nums[0];
+        //STRIVER'S A TO Z DSA COURSE
+
+        //METHOD 1:- BRUTE FORCE METHOD
+        //Generate all the sub arrays sum
+        //Using two loops (OPTIMIZED BRUTE FORCE)
+
+        /*
+        int maxSum= 0;
+        for (int i=0; i<nums.length; i++){
+            int sum= 0;
+            for (int j=i; j<nums.length; j++){
+                sum+= nums[j];
+                maxSum= Math.max(sum,maxSum);
+            }
+        }
+        return maxSum;
+         */
+        //TC is O(n^2) and Sc is O(1)
+        
+        //METHOD 2:- KADANE'S ALGORITHM
+        int sum=0, maxSum=Integer.MIN_VALUE;
         for (int num: nums){
-            sum+=num;
+            sum+= num;
             maxSum= Math.max(sum,maxSum);
             if (sum<0)
                 sum= 0;
         }
         return maxSum;
-
     }
 }
