1dcc0582932e4aa396d7f4c51e930f9dfef83a03
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-08-01 20:37:37 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (84.64%) - LeetSync

--- 2727-number-of-senior-citizens/number-of-senior-citizens.java
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.java
@@ -1,5 +1,7 @@
 class Solution {
     public int countSeniors(String[] details) {
+        //METHOD 1:- BRUTE FORCE METHOD
+        /*
         int count= 0;
         for (String str: details){
             int age= Integer.parseInt(str.substring(11,13));
@@ -7,5 +9,20 @@ class Solution {
                 count++;
         }
         return count;
+         */
+        //TC is O(N^2) AND SC is O(1)
+
+        //METHOD 2:- USING BRAIN
+        int count= 0;
+        for (String str: details){
+            char tens= str.charAt(11);
+            char ones= str.charAt(12);
+            
+            int age= (tens-'0')*10 + (ones-'0');
+            if (age>60)
+                count++;
+        }
+        return count;
+        //TC is O(N) AND SC is O(1)
     }
 }
