46ab95d7d6ae2110118b2956a4a25b7fa1273681
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-08-04 11:40:53 +0530

    Time: 64 ms (82.43%) | Memory: 46.1 MB (68.02%) - LeetSync

---
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int rangeSum(int[] nums, int n, int left, int right) {
+        //BRUTE FORCE APPROACH
+        int[] store= new int[(n*(n+1))/2];
+        int k=0;
+        for (int i=0; i<n; i++){
+            int sum= 0;
+            for (int j=i; j<n; j++){
+                sum+= nums[j];
+                store[k]= sum;
+                k++;
+            }
+        }
+
+        //Sort the store array in non-decreasing order.
+        Arrays.sort(store);
+
+        //left and right are 1-based indexing
+        int answer= 0;
+        long modulo= 1000000000+7;
+        for (int i=left-1; i<right; i++)
+            answer= (int) ((answer+= store[i])%modulo);
+        
+        return answer;
+        //TC is O(N^2) AND SC is O(N)
+    }
+}
