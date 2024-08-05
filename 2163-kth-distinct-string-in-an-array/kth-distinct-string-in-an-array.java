55ead4c98a5672df6cbcc577ceb621fd1db7a8b5
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-08-05 13:28:49 +0530

    Time: 7 ms (55.29%) | Memory: 44.5 MB (11.44%) - LeetSync

--- 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.java
+++ 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.java
@@ -1,6 +1,7 @@
 class Solution {
     public String kthDistinct(String[] arr, int k) {
         //METHOD 1:- BRUTE FORCE APPROACH
+        /*
         int count= 0;
         for (int i=0; i<arr.length; i++){
             int occurence= 0; //will count occurrence of arr[1
@@ -16,6 +17,24 @@ class Solution {
             }
         }
         return "";
+        */
         //TC is O(n^2) and SC is O(1)
+
+        //METHOD 2:-BY USING HASHMAP
+        LinkedHashMap<String, Integer> map= new LinkedHashMap<>();
+        //NOTE:- LinkedHashMap maintains the order of elements inserted into it while HashMap is unordered.
+        for (String str: arr){
+            map.put(str, map.getOrDefault(str,0)+1);
+        }
+        int count= 0;
+        for (String str: map.keySet()){
+            if (map.get(str)==1){
+                count++;
+                if (count==k)
+                    return str;
+            }
+        }
+        return "";
+        //TC is O(n) and SC is O(n)
     }
 }
