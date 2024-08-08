ca991409e70d26513472f15addf7761d73c1c13f
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-08-08 13:26:28 +0530

    Time: 17 ms (74.44%) | Memory: 43.8 MB (71.24%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -23,7 +23,9 @@ class Solution {
         //TC is O(n^4 * nlogn) and SC is O(1)
         //TLE
 
+
         //METHOD 2:- BETTER APPROACH
+        /*
         int n= nums.length;
         Set<List<Integer>> set= new HashSet<>();
         for (int i=0; i<n; i++){
@@ -47,6 +49,57 @@ class Solution {
             }
         }
         return new ArrayList<>(set);
+         */
         //TC is O(n^3 * nlogn) and SC is O(2 * no. of the quadruplets)+O(N)
+
+
+        //METHOD 3:- USING 3 POINTERS
+        List<List<Integer>> answer= new ArrayList<>();
+        Arrays.sort(nums);
+        int n= nums.length;
+
+        for (int i=0; i<n; i++){
+            //avoid the duplicates while moving i
+            if (i>0 && nums[i]==nums[i-1])
+                continue;
+            for (int j=i+1; j<n; j++){
+                //avoid the duplicates while moving j
+                if (j>i+1 && nums[j]==nums[j-1])
+                    continue;
+
+                int k= j+1, l=n-1;
+                while (k<l){
+                    //Using "long" to avoid Overflow because of constraints
+                    long sum= nums[i];
+                    sum+= nums[j];
+                    sum+= nums[k];
+                    sum+= nums[l];
+
+                    if (sum==target){
+                        List<Integer> list= new ArrayList<>();
+                        list.add(nums[i]);
+                        list.add(nums[j]);
+                        list.add(nums[k]);
+                        list.add(nums[l]);
+
+                        answer.add(list);
+
+                        k++; l--;
+
+                        //skip the duplicates
+                        while (k<l && nums[k-1]==nums[k])
+                            k++;
+                        while (k<l && nums[l]==nums[l+1])
+                            l--;
+                    }
+                    else if (sum<target)
+                        k++;
+                    else
+                        l--;
+                }
+            }
+        }
+        return answer;
+        //TC is O(n^3) and SC is O(no. of the quadruplets)
     }
 }
