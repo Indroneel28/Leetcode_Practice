cef1f434a4bdaa95652b319689741fda1b955608
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-08-09 23:11:56 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (86.24%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        //METHOD 1:- USING SORTING
+        //METHOD 1:- USING SORTING (BRAIN)
         /*
         for (int i = 0 , j = m ; i < n ; i++){
             nums1[j]= nums2[i];
@@ -11,6 +11,7 @@ class Solution {
         //TC is O(nlogn) and SC is O(1)
 
         //METHOD 2:- BRUTE FORCE APPROACH
+        /*
         int[] store= new int[m+n];
         int i= 0, j= 0, k= 0;
         while (i<m && j<n){
@@ -28,6 +29,25 @@ class Solution {
         for (int x=0; x<m+n; x++){
             nums1[x]= store[x];
         }
-        //TC is O(n) and SC is O(n)
+         */
+        //TC is O(2n) and SC is O(n)
+
+        //METHOD 3:- OPTIMAL SOLUTION
+        int i= m-1, j= n-1, k= m+n-1;
+        while (i>=0 && j>=0){
+            if (nums1[i]>nums2[j]){
+                nums1[k--]= nums1[i--];
+            }
+            else{
+                nums1[k--]= nums2[j--];
+            }
+        }
+        while (i>=0){
+            nums1[k--]= nums1[i--];
+        }
+        while (j>=0){
+            nums1[k--]= nums2[j--];
+        }
+        //TC is O(m+n) and SC is O(1)
     }
 }
