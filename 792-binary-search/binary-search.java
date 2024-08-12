dd77423feaf2bc8fc4dcf639f1a93d36b2792046
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-08-12 20:42:51 +0530

    Time: 0 ms (100.00%) | Memory: 45.5 MB (58.78%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,6 +1,7 @@
 class Solution {
     public int search(int[] nums, int target) {
         //METHOD 1:- ITERATIVE APPROACH
+        /*
         int low= 0, high= nums.length-1;
         while (low<=high){
             int mid =(low+high)/2;
@@ -14,6 +15,23 @@ class Solution {
             }
         }
         return -1;
+         */
         //TC is O(logn) and SC is O(1)
+        
+        //METHOD 2:- RECURSIVE APPROACH
+        return binarySearch(nums,target,0,nums.length-1);
+        //TC is O(logn) and SC is O(logn) due too stack space
+    }
+
+    private int binarySearch(int[] nums, int target, int low, int high){
+        if (low>high)
+            return -1;
+        int mid= (low+high)/2;
+        if (nums[mid]==target)
+            return mid;
+        else if (nums[mid]>target)
+            return binarySearch(nums,target,low,mid-1);
+        else
+            return binarySearch(nums,target,mid+1,high);
     }
 }
