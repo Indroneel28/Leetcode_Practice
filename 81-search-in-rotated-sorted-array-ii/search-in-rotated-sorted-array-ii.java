8933d4a0d492c01b792e27048d1c785ed24967a8
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-08-16 15:34:14 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (10.16%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,11 +1,53 @@
 class Solution {
     public boolean search(int[] nums, int target) {
         //METHOD 1:- LINEAR SEARCH
+        /*
         for (int num: nums){
             if (num==target)
                 return true; //target is present
         }
         return false; //target is not present
         //TC is O(n) and SC is O(1)
+         */
+
+        //METHOD 2:- BINARY SEARCH
+        int left= 0, right= nums.length-1;
+
+        while (left<=right){
+            int mid= (left+right)/2;
+
+            if (nums[mid]==target)
+                return true;
+
+            //New condition added as we can't decide which part is sorted
+            else if (nums[left]==nums[mid] && nums[mid]==nums[right]){
+                left++;
+                right--;
+            }
+
+            //If Left part is sorted
+            else if (nums[left]<=nums[mid]){
+                if (nums[left]<=target && target<=nums[mid]){
+                    right= mid-1;
+                }
+                else{
+                    left= mid+1;
+                }
+            }
+
+            //If Right part is sorted
+            else{
+                if (nums[mid]<=target && target<=nums[right]){
+                    left= mid+1;
+                }
+                else {
+                    right= mid-1;
+                }
+            }
+        }
+        
+        return false; //Element not found
+        //TC is O(n/2) in worst case because if array contain only duplicates then it will run n/2 times.
+        //SC is O(1)
     }
 }
