55d1a36f6107e9a845f25f5ebb3826bea32f740b
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-08-17 14:44:20 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (83.52%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,28 +1,21 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        // If the array contains multiple peaks, return the index to any of the peaks.
+        //STRIVER'S A TO Z
+        //METHOD 1:- LINEAR SEARCH
+        int n= nums.length;
+
+        //EDGE CASES
         if (nums.length==1)
             return 0;
-
-        int i=0, j= nums.length-1;
-        //For first element
         if (nums[0]>nums[1])
             return 0;
-        //For last element
-        else if (nums[nums.length-1]>nums[nums.length-2])
-            return nums.length-1;
-        //For remaining element
-        while (i<=j){
-            int mid= (i+j)/2;
-            if (nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1])
-                return mid;
-
-            else if (nums[mid]<nums[mid+1])
-                i= mid+1;
-            else
-                j= mid-1;
+        if (nums[n-1]>nums[n-2])
+            return n-1;
+        
+        for (int i=1; i<n-1; i++){
+            if (nums[i]>nums[i-1] && nums[i]>nums[i+1])
+                return i;
         }
-        return -1; //will never execute as for constraints
-
+        return -1; //Dummy statement
     }
 }
