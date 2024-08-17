fc747343eea5c91129259fc10e0350d412434f4f
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-08-17 17:23:46 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (92.94%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -27,14 +27,15 @@ class Solution {
         int n= nums.length;
 
         //EDGE CASES
-        if (nums.length==1)
+        if (n==1)
             return 0;
         if (nums[0]>nums[1])
             return 0;
         if (nums[n-1]>nums[n-2])
             return n-1;
 
-        int left= 0, right= n-1;
+        //int left= 0, right= n-1; //It's Correct NP
+        int left= 1, right= n-2; //Better to write (professional)
         while (left<=right){
             int mid= (left+right)/2;
             if (nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1])
