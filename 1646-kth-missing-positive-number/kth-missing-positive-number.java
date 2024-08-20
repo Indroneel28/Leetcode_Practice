658107ca949294a53c2aaf5071880ff478091164
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-08-20 13:50:23 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (20.12%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.java
@@ -21,6 +21,7 @@ class Solution {
         //TC is O(n^2) and SC is O(1)
 
         //METHOD 2:- BETTER SOLUTION
+        /*
         for (int ele: arr){
             if (ele<=k)
                 k++;
@@ -28,6 +29,21 @@ class Solution {
                 break;
         }
         return k;
+         */
         //TC is O(n) and SC is O(1)
+
+        //METHOD 3:- OPTIMAL SOLUTION
+        int left= 0, right= arr.length-1;
+        while (left<=right){
+            int mid= (left+right)/2;
+            int missing= arr[mid]-(mid+1);
+            if (missing<k)
+                left= mid+1;
+            else
+                right= mid-1;
+        }
+        //return k+right+1;
+        return k+left;
+        //TC is O(logn) and SC is O(1)
     }
 }
