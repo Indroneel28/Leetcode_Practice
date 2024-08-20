2e4314ca75345a70458fc62b5a82a891c6d0a11a
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-08-20 13:07:07 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (42.62%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.java
@@ -1,6 +1,7 @@
 class Solution {
     public int findKthPositive(int[] arr, int k) {
         //METHOD 1:- BRUTE FORCE METHOD
+        /*
         int count= 0, ans= 1;
         while (true){
             boolean found= false;
@@ -16,6 +17,17 @@ class Solution {
             }
             ans++;
         }
+         */
         //TC is O(n^2) and SC is O(1)
+
+        //METHOD 2:- BETTER SOLUTION
+        for (int ele: arr){
+            if (ele<=k)
+                k++;
+            else
+                break;
+        }
+        return k;
+        //TC is O(n) and SC is O(1)
     }
 }
