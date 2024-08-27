4f3064ce363b3950ee0ec8cdd776e270e68ba78d
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-08-27 17:02:57 +0530

    Time: 6 ms (57.05%) | Memory: 46 MB (27.06%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -10,10 +10,11 @@ class Solution {
         }
         return false;
          */
-        //TC os O(n*m) or O(rows*cols) and SC is O(1)
+        //TC is O(n*m) or O(rows*cols) and SC is O(1)
 
         //METHOD 2:- BETTER APPROACH
         //Binary Search in each row
+        /*
         int cols= matrix[0].length;
         for (int[] row: matrix){
             int left= 0, right= cols-1;
@@ -28,6 +29,22 @@ class Solution {
             }
         }
         return false;
+         */
         //TC is O(rows*log(cols)) and SC is O(1)
+
+        //METHOD 3:- USING TRICK
+        int row= 0, col= matrix[0].length-1; //col = number of columns - 1
+        //We are at top-right corner.
+
+        while (row<matrix.length && col>=0){
+            if (matrix[row][col]==target)
+                return true;
+            else if (matrix[row][col]<target)
+                row++;
+            else
+                col--;
+        }
+        return false;
+        //TC is O(rows+cols) and SC is O(1)
     }
 }
