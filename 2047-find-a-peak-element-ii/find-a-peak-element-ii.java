01d050541d9861dfd504bfb5d6b2be0be6ddf314
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-08-28 13:16:08 +0530

    Time: 0 ms (100.00%) | Memory: 90.2 MB (10.97%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] findPeakGrid(int[][] mat) {
         //METHOD 1:- BRUTE FORCE APPROACH
+        /*
         int rows= mat.length, cols= mat[0].length;
         int[] answer= new int[2];
         answer[0]= -1; answer[1]= -1;
@@ -20,7 +21,7 @@ class Solution {
                 //LEFT
                 if (j==0 || mat[i][j]>mat[i][j-1])
                     count++;
-                
+
                 //COUNT
                 if (count==4) {
                     answer[0] = i;
@@ -30,6 +31,39 @@ class Solution {
             }
         }
         return answer;
+         */
         //TC is O(rows*cols) and SC is O(2)
+
+        //METHOD 2:- OPTIMAL APPROACH
+        int left= 0, right= mat[0].length;
+        while (left<=right){
+            int mid= (left+right)/2; //mid is the col
+            int row= maxElement(mat,mid);
+
+            //CHECKING LEFT AND RIGHT
+            int leftElement= mid>0 ? mat[row][mid-1] : -1;
+            int rightElement= mid<mat[0].length-1 ? mat[row][mid+1] : -1;
+            if (mat[row][mid]>leftElement && mat[row][mid]>rightElement){
+                return new int[]{row,mid};
+            }
+            else if (leftElement > mat[row][mid])
+                right= mid-1; //Move Left
+            else
+                left= mid+1; //Move right
+        }
+        return new int[]{-1,-1}; //dummy statement
+        //TC is O(rows*log(cols)) and SC is O(2)
+    }
+
+    private int maxElement(int[][] arr, int col){
+        int row= -1, maxElement= Integer.MIN_VALUE;
+        //CHECKING UP TO DOWN
+        for (int i=0; i<arr.length; i++){ //Explore all rows and check
+            if (arr[i][col]>maxElement){
+                maxElement= arr[i][col];
+                row= i;
+            }
+        }
+        return row;
     }
 }
