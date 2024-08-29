44a770edc3114469be9bf12dd94d583515733b78
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-08-29 23:45:11 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (18.44%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.java
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.java
@@ -11,8 +11,6 @@ class Solution {
         node.val= node.next.val;
         node.next= node.next.next;
         //REMEMBER
-        //Question should be delete value not node
-        //This code is perfect
         //The value of each node in the list is unique.
         //The node to be deleted is in the list and is not a tail node.
     }
