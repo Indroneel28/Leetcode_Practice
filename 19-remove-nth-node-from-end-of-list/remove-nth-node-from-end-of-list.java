76309fbc14f00bf7ed5ec7ee909d8c1ac57684b9
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-08-31 21:53:29 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (61.25%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -1,54 +1,36 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     nt val;
+ *     int val;
  *     ListNode next;
- *     ListNode() {}i
+ *     ListNode() {}
  *     ListNode(int val) { this.val = val; }
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        //BEGINNER METHOD
-        
-        //First let's count the total number of nodes in the linked list
-        int count=0;
-        ListNode p= head;
-        while (p != null){
-            p= p.next;
+        //BRUTE FORCE METHOD
+        //Count the number of nodes
+        int count= 0;
+        for (ListNode i=head; i!=null; i= i.next)
             count++;
-        }
-        //If n==count means head of the LL is required to remove
+
+        //Edge case:- Delete counth from right => Delete 1st Node i.e. head
         if (count==n)
             return head.next;
 
-        p= head;
-        //Will traverse till n-1 nodes then we will remove the node at n (from last)
-        while (count-1>n){
-            p= p.next;
-            count--;
-        }
-        if (p.next!=null)
-            p.next= p.next.next;
-        return head;
+        //Difference
+        int dif= count-n;
 
-        //Optimized Code
-        // ListNode front= head;
-        // ListNode back= head;
-        // int x= n;
-        // while (x>0){
-        //     front= front.next;
-        //     x--;
-        // }
-        // if (front==null){
-        //     return head.next;
-        // }
-        // while (front.next!=null){
-        //     front= front.next;
-        //     back= back.next;
-        // }
-        // back.next= back.next.next;
-        // return head;
+        ListNode prev= head;
+        //Iterate before diff-1
+        while (dif>1){
+            dif--;
+            prev= prev.next;
+        }
+        prev.next= prev.next.next;
+        return head;
+        //TC is O(count+ (count-n)) and SC is O(1)
     }
 }
