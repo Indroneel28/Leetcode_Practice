b0da4dbad992b396a7342fb578007ae30a0bd147
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-09-13 20:39:14 +0530

    Time: 2 ms (100.00%) | Memory: 56 MB (78.90%) - LeetSync

--- 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.java
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] xorQueries(int[] arr, int[][] queries) {
         //BRUTE FORCE METHOD
+        /*
         int[] answer= new int[queries.length]; //queries size = answer array size
 
         int k=0;
@@ -13,6 +14,27 @@ class Solution {
         }
 
         return answer;
+         */
         //TC is O(queries.length*arr.length) and SC is O(queries.length)
+
+        //OPTIMAL METHOD:- Using Prefix xor
+        int xor= 0;
+        for (int i=0; i<arr.length; i++){
+            xor^=arr[i];
+            arr[i]= xor; //Each index i of arr will store xor from index 0 to index i
+        }
+
+        int[] answer= new int[queries.length]; // query size = answer array size
+        int k= 0;
+        for (int[] query: queries){
+            int start= query[0], stop= query[1];
+            if (start==0) //-1 index does not exist
+                answer[k]= arr[stop]; //from index start=0 to stop
+            else
+                answer[k]= arr[start-1]^arr[stop]; //from index start>0 to stop
+            k++;
+        }
+        return answer;
+        //TC is O(arr.length+queries.length) and SC is O(queries.length)
     }
 }
