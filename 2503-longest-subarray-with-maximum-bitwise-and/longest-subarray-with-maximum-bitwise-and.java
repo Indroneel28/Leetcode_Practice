2b63c756001a005f3935f42a838421ffda0a8465
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-09-14 11:41:07 +0530

    Time: 3 ms (89.89%) | Memory: 61.5 MB (5.06%) - LeetSync

--- 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.java
+++ 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.java
@@ -1,24 +1,22 @@
 class Solution {
     public int longestSubarray(int[] nums) {
-        //OPTIMAL METHOD:- Find the maximum element and it's consecutive occurrences
-        int maxOccur= nums[0], maxCount= 1, count= 1;
-        int prev= nums[0];
+        //OPTIMAL METHOD:- Find the maximum element and it's consecutive occurrences (maxStreak)
+        int maxOccur= nums[0], maxStreak= 1, streak= 1;
+
         for (int i=1; i< nums.length; i++ ){
-            if (nums[i]>prev && nums[i]>maxOccur) {
+            if (nums[i]>maxOccur) { //New max element
                 maxOccur = nums[i];
-                count= 1;
-                maxCount= 1;
+                streak= maxStreak= 1;
             }
-            else if (nums[i]>prev && nums[i]>=maxOccur){
-                count=1;
-            }
-            else if (nums[i]==prev && nums[i]==maxOccur) {
-                count++;
-            }
-            maxCount = Math.max(count, maxCount);
-            prev= nums[i];
+            else if (nums[i]==maxOccur) //same occurrence again
+                streak++;
+            else
+                streak= 0; //make current streak= 0
+            maxStreak = Math.max(streak, maxStreak);
         }
-        return maxCount;
+        return maxStreak;
         //TC is O(n) and SC is O(1)
+
+        //THIS IS BETTER SOLUTION AS COMPARED TO OTHERS BECUASE IN ONE FOR LOOP IT"S SOLVED
     }
 }
