64b9d4e23e544e11d571d6efe9c2b962baf879a0
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-09-17 10:59:49 +0530

    Time: 2 ms (100.00%) | Memory: 42 MB (69.28%) - LeetSync

--- 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.java
+++ 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.java
@@ -1,11 +1,12 @@
 class Solution {
     public String[] uncommonFromSentences(String s1, String s2) {
-        //USING HASHSET
+        //METHOD 1:- USING HASHSET
+        /*
         String[] list1= s1.split(" ");
         String[] list2= s2.split(" ");
 
-        HashSet<String> common= new HashSet<>();
-        HashSet<String> uncommon= new HashSet<>();
+        HashSet<String> common= new HashSet<>(); //To store common elements
+        HashSet<String> uncommon= new HashSet<>(); //To store uncommon elements
 
         //Insert all the words from list1 into the set
         for (String item: list1){
@@ -32,6 +33,32 @@ class Solution {
             answer[i++]= uniqueWord;
         }
         return answer;
+        //TC is O(2(m+n)) and SC is O(m+n+(m+n)+(m+n))
+        //WE COULD DIRECTLY SOLVE USING HASHMAP BUT I TRIED TO DO IT WITH HASHSET TO LEARN AND PRACTICE MORE!!
+        //BEAT 100%
+         */
+
+        //METHOD 2:- USING HASHMAP
+        //Count all the occurrences of the words
+        HashMap<String,Integer> map= new HashMap<>();
+
+        String[] list1= s1.split(" ");
+        String[] list2= s2.split(" ");
+
+        //Put all the words from both arrays into HashMap
+        for (String word: list1){
+            map.put(word,map.getOrDefault(word,0)+1);
+        }
+        for (String word: list2){
+            map.put(word,map.getOrDefault(word,0)+1);
+        }
+
+        ArrayList<String> list= new ArrayList<>();
+        for (String word: map.keySet()){
+            if (map.get(word)==1)
+                list.add(word);
+        }
+        return list.toArray(new String[0]);
         //TC is O(2(m+n)) and SC is O(m+n+(m+n)+(m+n))
     }
 }
