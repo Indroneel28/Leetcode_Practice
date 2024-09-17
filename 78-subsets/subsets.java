4477d4b20cb2a8006c3769de2768b6ddb1ba80de
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-09-17 21:00:16 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (6.57%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,17 +1,21 @@
 class Solution {
+    //METHOD 1:- USING RECURSION AND BACKTRACKING
     public List<List<Integer>> subsets(int[] nums) {
-        ArrayList<List<Integer>> answer= new ArrayList<>();
-        traverse(answer,new ArrayList<>(),nums,0);
-        return new ArrayList<>(answer);
+        List<List<Integer>> answer= new ArrayList<>();
+        traverse(answer,nums,new ArrayList<>(),0);
+        return answer;
     }
-    private void traverse(ArrayList<List<Integer>> answer, List<Integer> list, int[] nums, int index){
-        if (index==nums.length){
+    //TC is O(2^n) and SC is O(n)
+
+    //Recursion and Backtracking
+    private void traverse(List<List<Integer>> answer, int[] nums, List<Integer> list, int index){
+        if (index==nums.length) {
             answer.add(new ArrayList<>(list));
             return;
         }
         list.add(nums[index]);
-        traverse(answer,list,nums,index+1);
-        list.remove(list.size()-1);
-        traverse(answer,list,nums,index+1);
+        traverse(answer,nums,list,index+1);
+        list.remove(list.size()-1); //Remove the last inserted element //BACKTRACK
+        traverse(answer,nums,list,index+1);
     }
 }
