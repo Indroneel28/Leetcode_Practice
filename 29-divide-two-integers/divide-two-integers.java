92abc0626cad6ff8dc80bc5205f54288dc1bea19
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-09-17 20:32:48 +0530

    Time: 1 ms (71.74%) | Memory: 40.9 MB (47.43%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.java
+++ 29-divide-two-integers/divide-two-integers.java
@@ -1,7 +1,28 @@
 class Solution {
     public int divide(int dividend, int divisor) {
-        if(dividend== -2147483648 && divisor==-1)
-            return 2147483647;
-        return dividend/divisor;
+        //METHOD 1:- USING / OPERATOR
+        //return dividend/divisor;
+
+        //METHOD 2:- USING OPTIMA METHOD
+        //Edge cases
+        //Here, quotient will overflow so better to handle it
+        if (dividend==Integer.MIN_VALUE && divisor==-1)
+            return Integer.MAX_VALUE;
+
+        boolean negative= dividend < 0 ^ divisor < 0;
+
+        dividend= Math.abs(dividend);
+        divisor= Math.abs(divisor);
+
+        int quotient= 0, subQuot= 0;
+
+        while (dividend - divisor >= 0) {
+            for (subQuot = 0; dividend - (divisor << subQuot << 1) >= 0; subQuot++);
+            quotient += 1 << subQuot; //Add to the quotient
+            dividend -= divisor << subQuot; //Substract from dividend to start over with the remaining
+        }
+        return negative ? -quotient : quotient;
+        //TC is O(divident) and SC is O(1) in worst case
+        //CAN'T UNDERSTAND
     }
 }
