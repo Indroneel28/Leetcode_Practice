df1be9e3e6dca8f36852227212615c211cd369eb
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-09-18 19:26:21 +0530

    Time: 5 ms (98.24%) | Memory: 43.7 MB (29.35%) - LeetSync

--- 179-largest-number/largest-number.java
+++ 179-largest-number/largest-number.java
@@ -6,6 +6,8 @@ class Solution {
             list.add(Integer.toString(num));
         }
 
+        //Explanation for Comparator
+        /*
         list.sort((a, b) -> {
             StringBuilder sb1 = new StringBuilder();
             sb1.append(a);
@@ -25,6 +27,10 @@ class Solution {
             }
             return 0; //sb1 is same as sb2 => a and b are same
         });
+         */
+
+        //One liner Comparator using Lambda function
+        list.sort((a,b)-> (b+a).compareTo(a+b));
 
         StringBuilder sb = new StringBuilder();
         for (String s : list)
@@ -32,7 +38,8 @@ class Solution {
 
         if (sb.charAt(0) == '0')//For "00" , "000" , ... edge cases
             return "0";
-        return sb.toString(); //If any char is not 0 means sb.toString() is our answer    
-        // TC is O(n + nlogn + n) and SC is O(n)
+        return sb.toString(); //If any char is not 0 means sb.toString() is our answer
+        // TC is O(n + k*nlogn + n) and SC is O(n)
+        //The comparison function takes O(k) time, where k is the maximum number of digits in the numbers
     }
 }
