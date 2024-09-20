59dbc9425824bf1c89885d74bf5a5d0c81aff9de
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-09-20 22:08:01 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (56.04%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,21 +1,28 @@
 class Solution {
     public double myPow(double x, int n) {
-        //OPTIMAL METHOD:- RECURSIVE APPROACH
+        //OPTIMAL METHOD:- ITERATIVE APPROACH
 
         //sucking edge case (just ignore it)
-        if (n == Integer.MIN_VALUE) 
+        if (n == Integer.MIN_VALUE)
             return 1/x * myPow(x, n + 1); //ignore it
 
-        if (n<0){ //n is negative
-            n= -n; //make n positive
-            x= 1.0/x; 
+        if(n<0){ //power is negative
+            n= -n; //make power positive
+            x= 1.0/x;
         }
-        if (n==0)
-            return 1;
-        else if (n%2==0)
-            return myPow(x*x,n/2);
-        else //n%2==1
-            return x * myPow(x*x,n/2);
-        //TC is O(logn) and SC is O(logn)
+
+        double ans= 1.0;
+        while (n!=0){
+            if (n%2==1){
+                ans *= x;
+                n = n-1;
+            }
+            else { //n%2==0
+                x *= x;
+                n = n/2;
+            }
+        }
+        return ans;
+        //TC is O(logn) and SC is O(1)
     }
 }
