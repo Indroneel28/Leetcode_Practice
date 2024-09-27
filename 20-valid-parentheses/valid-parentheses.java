61adabf831e7a4d1c02245a29e53dad175cd9d61
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-09-27 17:20:57 +0530

    Time: 1 ms (98.56%) | Memory: 41.3 MB (84.18%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,17 +1,21 @@
 class Solution {
     public boolean isValid(String s) {
+        //METHOD 1:- Using Stack
         Stack<Character> stack= new Stack<>();
         for (int i=0; i<s.length(); i++){
             char c= s.charAt(i);
-            if (c=='(')
-                stack.push(')');
-            else if (c=='{')
-                stack.push('}');
-            else if (c=='[')
-                stack.push(']');
-            else if (stack.isEmpty() || stack.pop()!=c)
-                return false;
+            if (c=='(' || c=='{' || c=='[')
+                stack.push(c);
+            else {
+                if (stack.isEmpty())
+                    return false;
+                else if ((c==')' && stack.peek()=='(') || (c=='}' && stack.peek()=='{') || (c==']' && stack.peek()=='[')) 
+                    stack.pop();
+                else
+                    return false;
+            }
         }
         return stack.isEmpty();
+        //TC is O(n) and SC is O(n)
     }
 }
