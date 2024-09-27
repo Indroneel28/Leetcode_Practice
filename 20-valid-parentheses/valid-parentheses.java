090f895929e19bcadfe98fd34330fa51e175f463
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-09-27 19:19:49 +0530

    Time: 2 ms (52.51%) | Memory: 41.7 MB (24.78%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean isValid(String s) {
-        //METHOD 1:- Using Stack
+        //METHOD 1:- Using Stack by inserting starting brackets
+        /*
         Stack<Character> stack= new Stack<>();
         for (int i=0; i<s.length(); i++){
             char c= s.charAt(i);
@@ -9,11 +10,30 @@ class Solution {
             else {
                 if (stack.isEmpty())
                     return false;
-                else if ((c==')' && stack.peek()=='(') || (c=='}' && stack.peek()=='{') || (c==']' && stack.peek()=='[')) 
+                else if ((c==')' && stack.peek()=='(') || (c=='}' && stack.peek()=='{') || (c==']' && stack.peek()=='[')) {
                     stack.pop();
+                }
                 else
                     return false;
             }
+        }
+        return stack.isEmpty();
+         */
+        //TC is O(n) and SC is O(n)
+        //Beat 98.56%
+
+        //METHOD 2:- Using Stack by inserting ending brackets
+        Stack<Character> stack= new Stack<>();
+        for (int i=0; i<s.length(); i++){
+            char c= s.charAt(i);
+            if (c=='(')
+                stack.push(')');
+            else if (c=='{')
+                stack.push('}');
+            else if (c=='[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop()!=c)
+                return false;
         }
         return stack.isEmpty();
         //TC is O(n) and SC is O(n)
