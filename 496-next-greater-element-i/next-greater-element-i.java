8208ab949ccac388e40d59c17bcc522e4a3dc0ae
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-09-30 00:10:46 +0530

    Time: 3 ms (93.12%) | Memory: 44.3 MB (24.38%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,13 +1,13 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         //BRUTE FORCE METHOD
+        /*
         int[] arr= new int[nums1.length];
         for (int i=0; i<nums1.length; i++){
-            int ele= nums1[i];
-            int nge= -1;
+            int ele= nums1[i], nge= -1;
             boolean found= false;
             for (int j=0; j<nums2.length; j++){
-                if (ele==nums2[j])
+                if (ele==nums2[j]){
                     found= true;
                 if (found && nums2[j]>ele){
                     nge= nums2[j];
@@ -17,6 +17,30 @@ class Solution {
             arr[i]= nge;
         }
         return arr;
+         */
         //TC is O(n^2) and SC is O(1)
+
+        //OPTIMAL METHOD
+        HashMap<Integer,Integer> map= new HashMap<>();
+        Stack<Integer> stack= new Stack<>();
+        int[] ans= new int[nums1.length];
+
+        //Move from right to left in nums2 array
+        for (int i= nums2.length-1; i>=0; i--){
+            int ele= nums2[i], nge= -1;
+            while (!stack.isEmpty() && stack.peek()<=ele){
+                stack.pop();
+            }
+            if (!stack.isEmpty())
+                nge= stack.peek();
+            stack.push(ele);
+            map.put(ele,nge); //Will store ele and it's NGE
+        }
+
+        for (int i=0; i<nums1.length; i++){
+            ans[i]= map.get(nums1[i]);
+        }
+        return ans;
+        //TC is O(n+n+n) and SC is O(2n+n+n)
     }
 }
