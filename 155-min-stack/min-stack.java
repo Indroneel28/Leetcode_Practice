aaf62b8a276ea203df1662224e9e8ab1785a340a
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-09-29 12:45:10 +0530

    Time: 5 ms (38.81%) | Memory: 44.9 MB (45.56%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,55 +1,32 @@
-public class MinStack {
-
-    //METHOD 1:- USING ARRAY
-    /*
-    Methods pop, top and getMin operations will always be called on non-empty stacks. //Most IMP Line
-    At most 3 * 10^4 calls will be made to push, pop, top, and getMin.
-     */
-
-    //METHOD 2:- USING LINKEDLIST
-
-    //METHOD 3:- USING STACK AND Pair Class
-    Stack<Pair> stack;
+class MinStack {
+    //METHOD 4:- USING TWO STACKS
+    Stack<Integer> stack, minStack;
     public MinStack() {
         this.stack= new Stack<>();
+        this.minStack= new Stack<>();
     }
 
     public void push(int val) {
-        if (stack.isEmpty())
-            stack.push(new Pair(val,val));
-        else
-            stack.push(new Pair(val,Math.min(val,stack.peek().minValue)));
+        stack.push(val);
+        if (minStack.isEmpty() || val<=minStack.peek())
+            minStack.push(val);
     }
 
     public void pop() {
-        stack.pop();
+        int top= stack.pop();
+        if (minStack.peek()==top)
+            minStack.pop();
     }
 
     public int top() {
-        return stack.peek().value;
+        return stack.peek();
     }
 
     public int getMin() {
-        return stack.peek().minValue;
+        return minStack.peek();
     }
-    //TC is O(1) and SC is O(2n) coz we are storing pairs
+    //TC is O(1) and SC is O(2n)
 }
-
-//Pair Class
-class Pair{
-    int value; //Will store the value
-    int minValue; //Will store the minimum till now
-
-    Pair next; //Point to the next Pair
-
-    //Constructor
-    public Pair(int value, int minValue){
-        this.value= value;
-        this.minValue= minValue;
-        this.next= null;
-    }
-}
-
 
 /**
  * Your MinStack object will be instantiated and called as such:
