cc869e3f963411cc98fb79c1d472dcfe08ab4b5e
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-09-30 19:58:12 +0530

    Time: 17 ms (49.70%) | Memory: 46 MB (60.32%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
         //BRUTE FORCE METHOD:- USING PROPERTIES OF CIRCULAR ARRAY
+        /*
         int[] ans= new int[nums.length];
         for (int i=0; i<nums.length; i++){ //run n times
             int nge= -1;
@@ -14,6 +15,24 @@ class Solution {
             ans[i]= nge;
         }
         return ans;
+         */
         //TC is O(n^2) and SC is O(n)
+
+        //OPTIMAL METHOD :- USING MONOTONIC STACK
+        int[] ans= new int[nums.length];
+        Stack<Integer> stack= new Stack<>();
+        //We will run loop 2n times
+        for (int i=2*nums.length-1; i>=0; i--){
+            int index= i%nums.length; //Find index
+            int nge= -1; //next greater element
+            while (!stack.isEmpty() && stack.peek()<=nums[index])
+                stack.pop();
+            if (!stack.isEmpty())
+                nge= stack.peek();
+            ans[index]= nge; //store nge
+            stack.push(nums[index]);
+        }
+        return ans;
+        //TC is O(4n) and SC is O(n+n)
     }
 }
