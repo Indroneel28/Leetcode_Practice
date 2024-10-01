e38e57b3e9e33c7db65e14a4104edd6ff61930fc
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-10-01 19:52:19 +0530

    Time: 5 ms (93.45%) | Memory: 45.5 MB (49.44%) - LeetSync

--- 1497-design-a-stack-with-increment-operation/design-a-stack-with-increment-operation.java
+++ 1497-design-a-stack-with-increment-operation/design-a-stack-with-increment-operation.java
@@ -1,14 +1,17 @@
+//METHOD 2:- USING LAZY PROPAGATION
 class CustomStack {
     int top;
     int[] arr;
+    int[] increment;
     public CustomStack(int maxSize) {
         this.top= -1;
         this.arr= new int[maxSize];
+        this.increment= new int[maxSize];
         //TC is O(1)
     }
 
     public void push(int x) {
-        if (top+1 == arr.length)
+        if (top== arr.length-1)
             return;
         arr[++top]= x;
         //TC is O(1)
@@ -17,14 +20,28 @@ class CustomStack {
     public int pop() {
         if (top==-1)
             return -1;
-        return arr[top--];
+
+        int answer= increment[top]+arr[top];
+        if (top>0){
+            increment[top-1]+= increment[top]; //Lazy Propagation
+        }
+        increment[top]= 0;
+        arr[top]= 0;
+        top--;
+        return answer;
         //TC is O(1)
     }
 
     public void increment(int k, int val) {
-        for (int i=0; i<Math.min(k,top+1); i++)
-            arr[i]+=val;
-        //TC is O(k)
+        if (top==-1)
+            return;
+        else if (k>top+1){
+            increment[top]+=val;
+        }
+        else {
+            increment[k-1]+=val;
+        }
+        //TC is O(1)
     }
 }
 
