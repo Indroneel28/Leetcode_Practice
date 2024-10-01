f88dc21f92112fba8c4156fa4cc6edda289a0a63
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-10-01 22:02:00 +0530

    Time: 42 ms (23.87%) | Memory: 59.9 MB (7.41%) - LeetSync

--- 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.java
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.java
@@ -29,24 +29,18 @@ class Solution {
         HashMap<Integer,Integer> map= new HashMap<>();
         //Perform 2 sum
         for (int ele: arr){
-            ele= (ele%k + k) % k; //handle negative numbers also
-            if (map.containsKey((k-ele)%k)){ 
-                if (map.get((k-ele)%k)>1) //Occur more than 1 time
-                    map.put((k-ele)%k, map.get((k-ele)%k)-1); //Remove 1 occurrence
+            int rem= (ele%k + k) % k; //handle negative numbers also
+            int comp= (k-rem) % k;
+            if (map.containsKey(comp)){
+                if (map.get(comp)>1) //Occur more than 1 time
+                    map.put(comp, map.get(comp)-1); //Remove 1 occurrence
                 else //Occur only once
-                    map.remove((k-ele)%k); //Remove that element
+                    map.remove(comp); //Remove that element
             }
             else
-                map.put(ele,map.getOrDefault(ele,0)+1);
-        }
-        System.out.println(map);
-        if (map.containsKey(0)) { //For 5,10,15,20 if k= 5
-            if (map.get(0)%2==0)
-                map.remove(0);
-            else
-                return false;
+                map.put(rem,map.getOrDefault(rem,0)+1);
         }
         return map.isEmpty();
-        //TC is O(n) and SC is O(n)
+        //TC is O(n) and SC is O(k)
     }
 }
