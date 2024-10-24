4351e32ee39ea921bc8b5c7d879b107053ce43b8
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-10-24 15:24:18 +0530

    Time: 60 ms (74.73%) | Memory: 57.1 MB (73.86%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,51 +1,30 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        //OPTIMAL METHOD:-
-        //Step 1:- Find NSE index of array heights
-        int[] nse= nseFind(heights);
-        //Step 2:- Find PSE index of array hieghts
-        int[] pse= pseFind(heights);
-        //Step 3:- Calculate the area
+        //OPTIMAL METHOD
+        Stack<Integer> stack= new Stack<>();
         int maxArea= 0;
         for (int i=0; i<heights.length; i++){
-            //Area= base X height
-            int base= nse[i]-pse[i]-1; //Base
-            int height= heights[i]; //Height
-            int currArea= base*height; //Area
-            maxArea= Math.max(maxArea,currArea);
+            while (!stack.isEmpty() && heights[stack.peek()]>=heights[i]){
+                int index= stack.pop();
+                //While popping compute the area
+                int left= -1;
+                if (!stack.isEmpty())
+                    left= stack.peek();
+                int right= i;
+                maxArea= Math.max(maxArea,heights[index]*(right-left-1));
+            }
+            stack.push(i);
+        }
+        while (!stack.isEmpty()){
+            int index= stack.pop();
+            //While popping compute the area
+            int left= -1;
+            if (!stack.isEmpty())
+                left= stack.peek();
+            int right= heights.length;
+            maxArea= Math.max(maxArea,heights[index]*(right-left-1));
         }
         return maxArea;
-        //TC is O(2n+2n+n) and SC is O(2n+2n)
-    }
-    private int[] nseFind(int[] arr){
-        int[] nseIndex= new int[arr.length];
-        Stack<Integer> stack= new Stack<>();
-        for (int i=arr.length-1; i>=0; i--){
-            int nse= arr.length;
-            while (!stack.isEmpty() && arr[stack.peek()]>=arr[i])
-                stack.pop();
-            if (!stack.isEmpty())
-                nse= stack.peek();
-            nseIndex[i]= nse;
-            stack.push(i);
-        }
-        return nseIndex;
-        //TC is O(2n) and SC is O(2n)
-    }
-
-    private int[] pseFind(int[] arr){
-        int[] pseIndex= new int[arr.length];
-        Stack<Integer> stack= new Stack<>();
-        for (int i=0; i<arr.length; i++){
-            int pse= -1;
-            while (!stack.isEmpty() && arr[stack.peek()]>=arr[i])
-                stack.pop();
-            if (!stack.isEmpty())
-                pse= stack.peek();
-            pseIndex[i]= pse;
-            stack.push(i);
-        }
-        return pseIndex;
-        //TC is O(2n) and SC is O(2n)
+        //TC is O(2n) and SC is O(n)
     }
 }
