20e2ee1cd3bce44db204deb2d2ff8074b5ef16dd
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-10-28 20:58:10 +0530

    Time: 50 ms (33.65%) | Memory: 114.7 MB (52.07%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,6 +1,10 @@
 class LRUCache {
+    //WE can solve it using Doubly LinkedList + HashMap
+    //In Java, DLL + HashMap = LinkedHashMap
+
     LinkedHashMap<Integer,Integer> map;
     int size;
+
     public LRUCache(int capacity) {
         this.map= new LinkedHashMap<>();
         this.size= capacity;
@@ -8,23 +12,23 @@ class LRUCache {
 
     public int get(int key) {
         if (!map.containsKey(key))
-            return -1;
+            return -1; //key is not present
         int value= map.get(key);
         map.remove(key);
-        map.put(key,value);
+        map.put(key,value); //remove and update the key
         return value;
     }
 
     public void put(int key, int value) {
         if (map.containsKey(key)) {
             map.remove(key);
-            map.put(key, value);
+            map.put(key, value); //Bring back to the end(recently used part)
         }
         else if (map.size()<this.size){
             map.put(key,value);
         }
         else{
-            map.remove(map.keySet().iterator().next());
+            map.remove(map.keySet().iterator().next()); //To remove the LRU element
             map.put(key,value);
         }
     }
