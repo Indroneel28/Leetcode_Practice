f743b63271519347b37b76fba88f0d349fee879c
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-11-01 11:58:44 +0530

    Time: 5 ms (87.81%) | Memory: 44.3 MB (79.38%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,18 +1,18 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        //BETTER METHOD:- USING SLIDING WINDOW AND TWO POINTERS WITH HASHSET
-        HashSet<Character> set= new HashSet<>();
-        int left= 0, right= 0, maxLen= 0;
-        while (right<s.length()){
-            while (set.contains(s.charAt(right))){
-                set.remove(s.charAt(left));
-                left++;
+        //OPTIMAL METHOD:- USING SLIDING WINDOW AND TWO POINTERS WITH HASHMAP
+        HashMap<Character, Integer> map = new HashMap<>();
+        int left = 0, right = 0, maxLen = 0;
+        while (right < s.length()) {
+            if (map.containsKey(s.charAt(right))) {
+                left = Math.max(map.get(s.charAt(right)) + 1, left);
             }
-            set.add(s.charAt(right));
-            maxLen= Math.max(maxLen,right-left+1);
+            map.put(s.charAt(right), right);
+            maxLen = Math.max(maxLen, right - left + 1);
             right++;
+            
         }
         return maxLen;
-        //TC is O(2n) and SC is O(n)
+        //TC is O(n) and SC is O(2n) for HashMap
     }
 }
