99192838ef5dcc3cee58a586c29b3fc6b6ac3219
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-11-05 00:38:02 +0530

    Time: 10 ms (80.84%) | Memory: 55.2 MB (24.93%) - LeetSync

---
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numberOfSubarrays(int[] nums, int k) {
+        //OPTIMAL METHOD:- USING SLIDING WINDOW
+        return traverse(nums,k)-traverse(nums,k-1);
+        //TC is O(44n) and SC is O(1)
+    }
+    private int traverse(int[] nums, int k){
+        int left= 0, right= 0, countOdds= 0, subArrays= 0;
+        while (right<nums.length){
+            if ((nums[right]&1)==1) //Odd Number
+                countOdds++;
+            while (countOdds>k){
+                if((nums[left]&1)==1) //Odd Number
+                    countOdds--;
+                left++;
+            }
+            subArrays+= right-left+1; //nice subarray found
+            right++;
+        }
+        return subArrays;
+    }
+}
