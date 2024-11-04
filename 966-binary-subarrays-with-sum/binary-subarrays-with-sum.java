4c05da5ecaf0fba1268cdcec3c705f885feafd36
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-11-04 23:59:10 +0530

    Time: 3 ms (55.46%) | Memory: 47.6 MB (61.19%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,19 +1,25 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
+        //OPTIMAL METHOD
+        // sum==goal  =>  sum<=goal - sum<goal  =>  sum<=goal -sum<=goal-1
         return traverse(nums,goal)-traverse(nums,goal-1);
+        //TC is O(4n) and SC is O(1)
     }
-    private int traverse(int[] nums, int goal){
-        //USING TWO POINTERS METHOD
-        int start=0, sum=0, ans= 0;
 
-        for (int end=0; end<nums.length; end++){
-            sum+=nums[end];
-            while (sum>goal && start<=end){
-                sum-=nums[start];
-                start++;
+    //Helper Method
+    private int traverse(int[] nums, int goal){
+        if (goal<0)
+            return 0;
+        int left= 0, right= 0, sum= 0, countSubArrays= 0;
+        while (right<nums.length){
+            sum+=nums[right];
+            while (sum>goal){
+                sum-=nums[left];
+                left++;
             }
-            ans+=end-start+1;
+            countSubArrays+= (right-left+1);
+            right++;
         }
-        return ans;
+        return countSubArrays;
     }
 }
