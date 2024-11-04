b7b7dd151901419069f1f0adf2eb9a7c4978f382
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-11-04 21:31:24 +0530

    Time: 27 ms (27.68%) | Memory: 44.3 MB (28.70%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        //BETTER METHOD:- USING SLIDING WINDOW AND TWO POINTERS
+        int left= 0, right= 0, maxLen= 0, maxFreq= 0;
+        HashMap<Character,Integer> map= new HashMap<>();
+        while (right<s.length()){
+            map.put(s.charAt(right),map.getOrDefault(s.charAt(right),0)+1);
+            maxFreq= Math.max(maxFreq, map.get(s.charAt(right)));
+            while (right-left+1 - maxFreq > k){
+                char c= s.charAt(left);
+                if (map.get(c)==1)
+                    map.remove(c);
+                else
+                    map.put(c,map.get(c)-1);
+                
+                //Max Frequency can be reduced => Check again
+                maxFreq= 0;
+                for (int value: map.values()) //O(26) coz only 26 characters
+                    maxFreq= Math.max(maxFreq,value);
+                left++;
+            }
+            maxLen= Math.max(maxLen,right-left+1);
+            right++;
+        }
+        return maxLen;
+        //TC is O(2n) and SC is O(n)
+    }
+}
