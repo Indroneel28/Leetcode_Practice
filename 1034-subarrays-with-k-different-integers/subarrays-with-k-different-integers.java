0ae677a4c0fe349aaceed2e26186ce4ca1b02d2a
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-11-05 19:41:13 +0530

    Time: 39 ms (86.82%) | Memory: 47.3 MB (66.48%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -1,29 +1,25 @@
 class Solution {
     public int subarraysWithKDistinct(int[] nums, int k) {
-        int subWithMaxK = subarrayWithAtMostK(nums, k);
-        int reducedSubWithMaxK = subarrayWithAtMostK(nums, k - 1);
-        return subWithMaxK - reducedSubWithMaxK;
+        //OPTIMAL METHOD
+        return traverse(nums,k) - traverse(nums,k-1);
+        //TC is O(4n) and SC is O(2n)
     }
-    
-    public int subarrayWithAtMostK(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int left = 0, right = 0, ans = 0;
-        
-        while (right < nums.length) {
-            map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
-            
-            while (map.size() > k) {
-                map.put(nums[left], map.get(nums[left]) - 1);
-                if (map.get(nums[left]) == 0) {
-                    map.remove(nums[left]);
-                }
+    private int traverse(int[] nums, int k){
+        int left= 0, right= 0, subArray= 0;
+        Map<Integer,Integer> map= new HashMap<>();
+        while (right<nums.length){
+            map.put(nums[right],map.getOrDefault(nums[right],0)+1);
+            while (map.size()>k){
+                int rem= nums[left];
+                if (map.get(rem)==1)
+                    map.remove(rem);
+                else
+                    map.put(rem,map.get(rem)-1);
                 left++;
             }
-            
-            ans += right - left + 1; // Size of subarray
+            subArray += right-left+1;
             right++;
         }
-        
-        return ans;
+        return subArray;
     }
 }
