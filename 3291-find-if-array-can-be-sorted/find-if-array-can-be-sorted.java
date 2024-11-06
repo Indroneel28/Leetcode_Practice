a2a1660f85f06ab779e48aeb6c11005da5a05071
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-11-07 00:36:04 +0530

    Time: 4 ms (47.14%) | Memory: 44.4 MB (35.22%) - LeetSync

--- 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.java
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.java
@@ -4,27 +4,17 @@ class Solution {
         for (int i=0; i<nums.length-1; i++){
             for (int j=0; j<nums.length-1-i; j++){
                 if (nums[j+1]<nums[j]){
-                    if (countBits(nums[j+1])==countBits(nums[j])){
+                    if (Integer.bitCount(nums[j+1])==Integer.bitCount(nums[j])){
                         int temp= nums[j];
                         nums[j]= nums[j+1];
                         nums[j+1]=temp;
                     }
-                    else {
+                    else
                         return false;
-                    }
                 }
             }
         }
         return true;
-        //TC is O(N^2*256) and SC is O(1)
-    }
-    private int countBits(int num){
-        int count= 0;
-        while (num!=0){
-            int rem= num%2;
-            count+=rem;
-            num/=2;
-        }
-        return count;
+        //TC is O(n^2*256) and SC is O(1)
     }
 }
