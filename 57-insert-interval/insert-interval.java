7b0adf3845f709b61515c86a90988601366574ee
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-12-02 21:46:36 +0530

    Time: 1 ms (99.14%) | Memory: 45.5 MB (6.06%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,30 +1,28 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> result = new LinkedList<>();
-	    int i = 0;
-	    // add all the intervals ending before newInterval starts
-	    while (i < intervals.length && intervals[i][1] < newInterval[0]){
-	        result.add(intervals[i]);
-	        i++;
-	    }
-	    
-	    // merge all overlapping intervals to one considering newInterval
-	    while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
-	    	// we could mutate newInterval here also
-	        newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
-	        newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
-	        i++;
-	    }
-	    
-	    // add the union of intervals we got
-	    result.add(newInterval); 
-	    
-	    // add all the rest
-	    while (i < intervals.length){
-	    	result.add(intervals[i]); 
-	    	i++;
-	    }
-	    
-	    return result.toArray(new int[result.size()][]);
+        //GREEDY APPROACH
+        ArrayList<int[]> list= new ArrayList<>();
+        //This is ArrayList of Integer Array
+        int i=0;
+        while (i<intervals.length && intervals[i][1]<newInterval[0]){
+            list.add(intervals[i]);
+            i++;
+        }
+        
+        //Update newInterval if overlapping of newInterval is there with interval
+        while (i<intervals.length && intervals[i][0]<=newInterval[1]){
+            newInterval[0]= Math.min(newInterval[0],intervals[i][0]);
+            newInterval[1]= Math.max(newInterval[1],intervals[i][1]);
+            i++;
+        }
+        list.add(newInterval);
+
+        while (i<intervals.length && newInterval[1]<intervals[i][0]){
+            list.add(intervals[i]);
+            i++;
+        }
+
+        return list.toArray(new int[list.size()][]);
+        //TC is O(n) and SC is O(n)
     }
 }
