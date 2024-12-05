dff8efb7e5b6fed64662a02844099b9e00ae82a9
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-12-05 20:33:19 +0530

    Time: 15 ms (66.47%) | Memory: 45.4 MB (71.35%) - LeetSync

--- 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.java
+++ 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.java
@@ -2,52 +2,21 @@ class Solution {
     public boolean canChange(String start, String target) {
         //OPTIMAL METHOD:- USING TWO POINTERS
         int i=0, j=0;
-        int cl1=0, cl2=0, cr1=0, cr2=0;
-        while (i<start.length() && j<target.length()){
-            if (i<start.length() && start.charAt(i)=='_')
+        while (i<start.length() || j<target.length()){
+            while (i<start.length() && start.charAt(i)=='_')
                 i++;
-            else if (j<target.length() && target.charAt(j)=='_')
+            while (j<target.length() && target.charAt(j)=='_')
                 j++;
-            else{
-                char l = start.charAt(i), r = target.charAt(j);
-                if (l=='L')
-                    cl1++;
-                else if (l=='R')
-                    cr1++;
-                if (r=='L')
-                    cl2++;
-                else if (r=='R')
-                    cr2++;
 
-                if (start.charAt(i) == target.charAt(j)) {
-                    if (j > i && l == 'L')
-                        return false;
-                    else if (i > j && r == 'R')
-                        return false;
-                    else {
-                        i++;
-                        j++;
-                    }
-                }
-                else //char does not match at-all
-                    return false;
-            }
+            //If any index reaches the end
+            if (i==start.length() || j==target.length())
+                return i==start.length() && j==start.length();
+
+            if (start.charAt(i)!=target.charAt(j) || (i<j && start.charAt(i)=='L') || (j<i && start.charAt(i)=='R'))
+                return false;
+            i++; j++;
         }
-        while (i<start.length()){
-            if (start.charAt(i)=='L')
-                cl1++;
-            else if(start.charAt(i)=='R')
-                cr1++;
-            i++;
-        }
-        while (j<target.length()){
-            if (target.charAt(j)=='L')
-                cl2++;
-            else if (target.charAt(j)=='R')
-                cr2++;
-            j++;
-        }
-        return cl1==cl2 && cr1==cr2;
+        return true;
         //TC is O(m+n) and SC is O(1)
     }
 }
