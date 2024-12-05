714a4153604721459e693afa60257810265ddd24
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-12-05 23:45:22 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (58.72%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,17 +1,17 @@
 class Solution {
     public int climbStairs(int n) {
-        //THIS IS SAME LIKE FIB SERIES QUESTION
+        //THIS IS SAME LIKE FIB SERIES QUESTION BUT BASE CASES RE DIFFERENT
         //BETTER METHOD:- MEMOIZATION (TOP-DOWN APPROACH)
-        
+
         int[] dp= new int[n+1];
         Arrays.fill(dp,-1);
         return memoize(n,dp);
         //TC is O(n) and SC is O(2n)
     }
-    
+
     public int memoize(int n, int[] dp){
-        if (n<=1)
-            return 1;
+        if (n<=2) //THIS IS ALSO GOOD
+            return n;
         if (dp[n]!=-1)
             return dp[n]; //No need to calculate again
         return dp[n]= memoize(n-1,dp)+memoize(n-2,dp);
