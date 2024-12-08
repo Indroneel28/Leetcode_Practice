d00558cc1e973f1d547e99b41090488672f10d7e
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-12-08 12:37:51 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (41.28%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,18 +1,21 @@
 class Solution {
-    //METHOD 2:- MEMOIZATION
+    //METHOD 2:- MEMOIZATION (Top-down Approach)
+    //STRIVER's METHOD
     public int rob(int[] nums) {
         int[] dp= new int[nums.length];
         Arrays.fill(dp,-1);
-        return memoization(nums,dp,0);
+        return memoization(nums,dp,nums.length-1);
     }
     private int memoization(int[] nums, int[] dp, int index){
-        if (index>=nums.length)
-            return 0;
-        if (index==nums.length-1)
+        if (index==0)
             return nums[index];
+        if (index<0)
+            return 0;
         if (dp[index]!=-1)
             return dp[index];
-        return dp[index]= Math.max(nums[index]+memoization(nums,dp,index+2),nums[index+1]+memoization(nums,dp,index+3));
+        int pick= nums[index] + memoization(nums,dp,index-2);
+        int notPick= memoization(nums,dp,index-1);
+        return dp[index]= Math.max(pick,notPick);
     }
     //TC is O(n) and SC is O(2n)
 }
