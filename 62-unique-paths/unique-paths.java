6e70eb9cbb99297b08ba8375dda0774c882eebcb
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-12-13 01:40:35 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (22.83%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,19 +1,22 @@
 class Solution {
-    //METHOD 2:- MEMOIZATION (TOP-DOWN APPROACH)
+    //METHOD 3:- TABULATION (BOTTOM-UP APPROACH)
     public int uniquePaths(int m, int n) {
-        int[][] matrix= new int[m][n];
         int[][] dp= new int[m][n];
-        return memoization(matrix,dp,m-1,n-1);
-    }
 
-    public int memoization(int[][] matrix, int[][] dp, int i, int j){
-        if (i==0 && j==0)
-            return 1;
-        if (i<0 || j<0)
-            return 0;
-        if (dp[i][j]!=0)
-            return dp[i][j];
-        return dp[i][j]= memoization(matrix,dp,i-1,j) + memoization(matrix,dp,i,j-1);
+        //Goal:- Fill 0th column and 0th row with 1.
+        for (int i=0; i<dp.length; i++){
+            dp[i][0]= 1;
+        }
+        for (int i=0; i<dp[0].length; i++){
+            dp[0][i]= 1;
+        }
+
+        for (int i=1; i<dp.length; i++){
+            for (int j=1; j<dp[0].length; j++){
+                dp[i][j]= dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
     }
-    //TC is O(m*n) and SC is O(m+n+m*n)
+    //TC is O(m*n + m + n) and SC is O(m*n)
 }
