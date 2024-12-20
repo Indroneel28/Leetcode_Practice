dd483076b90a9bc0b3ae074df117d97b2efe6783
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-12-21 01:55:07 +0530

    Time: 1 ms (99.77%) | Memory: 48.1 MB (6.92%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,24 +1,16 @@
 class Solution {
-    //METHOD 1:- TABULATION (BTTOM-UP APPROACH)
+    //METHOD 2:- TOP-DOWN APPROACH (MEMOIZATION)
     public int minPathSum(int[][] grid) {
-        int[][] matrix= new int[grid.length][grid[0].length];
-        for (int i=0; i<grid.length; i++){
-            for (int j=0; j<grid[0].length; j++){
-                if (i==0 && j==0) {
-                    matrix[i][j] = grid[i][j];
-                }
-                else {
-                    int sum1 = Integer.MAX_VALUE;
-                    if (i > 0)
-                        sum1 = matrix[i - 1][j];
-                    int sum2 = Integer.MAX_VALUE;
-                    if (j > 0)
-                        sum2 = matrix[i][j - 1];
-                    matrix[i][j] = grid[i][j] + Math.min(sum1, sum2);
-                }
-            }
-        }
-        return matrix[grid.length-1][grid[0].length-1];
+        return memoization(grid,new int[grid.length][grid[0].length],grid.length-1,grid[0].length-1);
     }
-    //TC is O(m*n) and SC is O(m*n)
+    private int memoization(int[][] grid, int dp[][], int i, int j){
+        if (i==0 && j==0)
+            return grid[i][j];
+        if (i<0 || j<0)
+            return Integer.MAX_VALUE;
+        if (dp[i][j]!=0)
+            return dp[i][j];
+        return  dp[i][j]= grid[i][j] + Math.min(memoization(grid,dp,i-1,j),memoization(grid,dp,i,j-1));
+    }
+    //TC is O(m*n) and SC is O(m*n + m + n)
 }
