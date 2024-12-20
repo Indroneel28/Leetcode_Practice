d2746c986f8b5202b44f84357818c6b75e02829c
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-12-20 21:09:27 +0530

    Time: 8 ms (42.99%) | Memory: 50.7 MB (13.10%) - LeetSync

--- 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.java
+++ 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.java
@@ -14,37 +14,35 @@
  * }
  */
 class Solution {
-    //BFS METHOD
+    //BREADTH FIRST SEARCH
     public TreeNode reverseOddLevels(TreeNode root) {
         Queue<TreeNode> queue= new LinkedList<>();
         queue.add(root);
         boolean odd= true;
         while (!queue.isEmpty()){
             ArrayList<TreeNode> list= new ArrayList<>();
-
-            while (!queue.isEmpty()){
+            int size= queue.size();
+            for (int i=0; i<size; i++){
                 TreeNode node= queue.poll();
-                if(node.left!=null)
+                if (node.left!=null)
                     list.add(node.left);
-                if(node.right!=null)
+                if (node.right!=null)
                     list.add(node.right);
             }
-
-            if (odd && !list.isEmpty()){
-                for (int i=0; i<list.size()/2; i++){
-                    TreeNode node1= list.get(i);
-                    TreeNode node2= list.get(list.size()-1-i);
-                    
-                        int temp = node1.val;
-                        node1.val = node2.val;
-                        node2.val = temp;
-                    
+            if(odd){
+                int i=0, j=list.size()-1;
+                while (i<j){
+                    TreeNode node1= list.get(i), node2= list.get(j);
+                    int val= node1.val;
+                    node1.val= node2.val;
+                    node2.val= val;
+                    i++; j--;
                 }
             }
             queue.addAll(list);
-            list= new ArrayList<>();
-            odd= !odd; //For next iteration
+            odd= !odd;
         }
         return root;
     }
+    //TC is O(n) and SC is O(n)
 }
