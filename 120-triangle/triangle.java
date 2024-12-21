1f85dad353e0c969ce9b903166002be494a7e4cb
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-12-21 20:27:09 +0530

    Time: 5 ms (39.03%) | Memory: 44.3 MB (37.58%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,26 +1,18 @@
 class Solution {
-    //METHOD 2:- MEMOIZATION (TOP-DOWN APPROACH)
+    //METHOD 3:- TABULATION (BOTTOM-UP APPROACH)
     public int minimumTotal(List<List<Integer>> triangle) {
-        List<List<Integer>> dp= new ArrayList<>();
-        for (int i=0; i<triangle.size(); i++){
-            List<Integer> list= new ArrayList<>();
-            for (int j=0; j<triangle.get(i).size(); j++){
-                list.add(null);
+        int n= triangle.size();
+        List<Integer> list= new ArrayList<>(triangle.get(n-1));
+
+        for (int i=n-2; i>=0; i--){
+            for (int j=0; j<=i; j++) {
+                int left = list.get(j);
+                int right = list.get(j + 1);
+                list.set(j,triangle.get(i).get(j)+Math.min(left,right));
             }
-            dp.add(list);
+            list.remove(list.size()-1);
         }
-        return memoization(triangle,dp,0,0);
+        return list.get(0);
     }
-    private int memoization(List<List<Integer>> triangle, List<List<Integer>> dp, int i, int j){
-        if (i==triangle.size())
-            return 0;
-        if (j<0 || j>=triangle.get(i).size())
-            return Integer.MAX_VALUE;
-        if (dp.get(i).get(j)!=null)
-            return dp.get(i).get(j);
-        int mini= triangle.get(i).get(j) + Math.min(memoization(triangle,dp,i+1,j),memoization(triangle,dp,i+1,j+1));
-        dp.get(i).set(j,mini);
-        return mini;
-    }
-    //TC is O(n^2) and SC is O(n^2 + n)
+    //TC is O(n^2) and SC is O(n)
 }
