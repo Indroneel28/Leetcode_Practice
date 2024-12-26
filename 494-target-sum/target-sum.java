41f0e37734ce266b972d0a5ce40da5b535fc1bdf
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2024-12-27 01:25:00 +0530

    Time: 674 ms (6.63%) | Memory: 41.6 MB (65.90%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,9 +1,6 @@
 class Solution {
     //METHOD 1:- RECURSION
     public int findTargetSumWays(int[] nums, int target) {
-        int[][] dp= new int[nums.length][1000+1];
-        for (int[] row: dp)
-            Arrays.fill(row,-1);
         return recursion(nums,target,nums.length-1,0);
     }
     private int recursion(int[] nums, int target, int index, int sum){
