bfedd6f1c5cd6f6a28bd6146f902ab0b972a177b
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2025-01-03 10:35:34 +0530

    Time: 2 ms (100.00%) | Memory: 58.2 MB (73.24%) - LeetSync

--- 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.java
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.java
@@ -1,27 +1,19 @@
 class Solution {
     public int waysToSplitArray(int[] nums) {
-        //BETTER METHOD:- USING RIGHT ARRAY AND LEFT ARRAY
-        int n= nums.length;
-        long[] leftArray= new long[n];
-        long[] rightArray= new long[n];
-        
-        leftArray[0]= nums[0];
-        for (int i=1; i<n;i++){
-            leftArray[i]= leftArray[i-1]+nums[i];
-        }
-        rightArray[n-1]= nums[n-1];
-        for (int i=n-2; i>=0; i--){
-            rightArray[i]= rightArray[i+1]+nums[i];
-        }
+        //OPTIMAL METHOD:- OPTIMIZED PREFIX SUM WITHOUT USING ARRAY
+        long endSum= 0;
+        for (int ele: nums)
+            endSum+= ele;
 
+        long startSum= 0;
         int count= 0;
-        for (int i=0; i<n-1; i++){
-            if (leftArray[i]>=rightArray[i+1])
+        for (int i=0; i<nums.length-1; i++){
+            startSum+= nums[i];
+            endSum-= nums[i];
+            if (startSum>=endSum)
                 count++;
         }
         return count;
-        //TC is O(3n) and SC is O(2n)
-        
-        //NOTE:- We can further optimize it into TC= O(2n) and SC= O(n) by using only rightArray.
+        //TC is O(2n) and SC is O(1)
     }
 }
