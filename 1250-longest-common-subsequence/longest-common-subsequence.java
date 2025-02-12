ddcec2a797135d93d891aa24bae3f638555e8c31
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2025-02-12 20:01:24 +0530

    Time: 20 ms (76.40%) | Memory: 50.7 MB (82.27%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,21 +1,22 @@
 class Solution {
-    //METHOD 3:- MEMOIZATION (TOP-DOWN APPROACH)
+    //METHOD 4:- TABULATION (BOTTOM-UP APPROACH)
+    //We need shifting of index coz base case occurs at i=-1 or j=-1
     public int longestCommonSubsequence(String text1, String text2) {
-        int[][] dp= new int[text1.length()][text2.length()];
-        for (int[] row: dp)
-            Arrays.fill(row,-1);
-        return memoize(text1,text2,text1.length()-1,text2.length()-1,dp);
+        int m= text1.length(), n= text2.length();
+        int[][] dp= new int[m+1][n+1];
+        for (int i=0; i<=m; i++)
+            dp[i][0]= 0;
+        for (int j=0; j<=n; j++)
+            dp[0][j]= 0;
+        for (int i=1; i<=m; i++){
+            for (int j=1; j<=n; j++){
+                if (text1.charAt(i-1)==text2.charAt(j-1))
+                    dp[i][j]= 1 + dp[i-1][j-1];
+                else 
+                    dp[i][j]= Math.max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[m][n];
     }
-    //Helper Method
-    private int memoize(String text1, String text2, int i, int j, int[][] dp){
-        if (i<0 || j<0)
-            return 0;
-        if (dp[i][j]!=-1)
-            return dp[i][j];
-        if (text1.charAt(i)==text2.charAt(j))
-            return dp[i][j]= 1 + memoize(text1,text2,i-1,j-1,dp);
-        return dp[i][j]= Math.max(memoize(text1,text2,i-1,j,dp),memoize(text1,text2,i,j-1,dp));
-    }
-    //TC is O(m*n) and SC is O(m*n + (m+n)) 
-    // m+n extra space due to recursion stack
+    //TC is O(m*n) and SC is O(m*n)
 }
