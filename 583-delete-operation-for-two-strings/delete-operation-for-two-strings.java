87af56c16dcca722f20f108a40236b315546ceda
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2025-02-13 16:36:54 +0530

    Time: 7 ms (77.14%) | Memory: 45.2 MB (7.97%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -1,25 +1,17 @@
 class Solution {
-    //METHOD 2:- MEMOIZATION (TOP-DOWN APPROACH)
+    //METHOD 3:- TABULATION (BOTTOM-UP APPROACH) [OPTIMAL SOLUTION]
     public int minDistance(String word1, String word2) {
-        int[][] dp= new int[word1.length()][word2.length()];
-        for (int[] row: dp)
-            Arrays.fill(row,-1);
-        int lcs= memoize(word1,word2,word1.length()-1,word2.length()-1,dp);
-        
-        //minimum steps required to make word1 and word2 the same.
-        return (word1.length()-lcs) + (word2.length()-lcs);
-        //after performing this, s1 and s2 will become lcs of old s1 and old s2.
+        int n= word1.length(), m= word2.length();
+        int[][] dp= new int[n+1][m+1];
+        for (int i=1; i<=n; i++){
+            for (int j=1; j<=m; j++){
+                if (word1.charAt(i-1)==word2.charAt(j-1))
+                    dp[i][j]= 1 + dp[i-1][j-1];
+                else 
+                    dp[i][j]= Math.max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return (n-dp[n][m]) + (m-dp[n][m]);
     }
-    //Helper Method
-    private int memoize(String word1, String word2, int i, int j, int[][] dp){
-        if (i<0 || j<0)
-            return 0;
-        if (dp[i][j]!=-1)
-            return dp[i][j];
-        if (word1.charAt(i)==word2.charAt(j))
-            return dp[i][j]= 1 + memoize(word1,word2,i-1,j-1,dp);
-        //else
-        return dp[i][j]= Math.max(memoize(word1,word2,i-1,j,dp),memoize(word1,word2,i,j-1,dp));
-    }
-    //TC is O(m*n) and SC is O(m*n + (m+n)
+    //TC is O(m*n) and SC is O(m*n)
 }
