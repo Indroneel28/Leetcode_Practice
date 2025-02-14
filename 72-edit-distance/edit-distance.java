8289bc611228145fdf44ebe37e58cb0724d3dc63
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2025-02-15 02:04:48 +0530

    Time: 5 ms (64.35%) | Memory: 45.1 MB (61.48%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,27 +1,22 @@
 class Solution {
-    //METHOD 1:- MEMOIZATION (TOP-DOWN APPROACH)
+    //METHOD 2:- TABULATION (BOTTOM-UP APPROACH)  [OPTIMAL SOLUTION]
     public int minDistance(String word1, String word2) {
-        int[][] dp= new int[word1.length()][word2.length()];
-        for (int[] row: dp)
-            Arrays.fill(row,-1);
-        return memoize(word1,word2,word1.length()-1,word2.length()-1,dp);
-    }
-    //Helper Method
-    private int memoize(String word1, String word2, int i, int j, int[][] dp){
-        if (i<0)
-            return j+1;
-        if (j<0)
-            return i+1;
-        if (dp[i][j]!=-1)
-            return dp[i][j];
-        if (word1.charAt(i)==word2.charAt(j))
-            return dp[i][j]= memoize(word1,word2,i-1,j-1,dp);
-        else{
-            int insertion= 1 + memoize(word1,word2,i,j-1,dp);
-            int deletion= 1 + memoize(word1,word2,i-1,j,dp);
-            int replace= 1 + memoize(word1,word2,i-1,j-1,dp);
-            return dp[i][j]= Math.min(insertion,Math.min(deletion,replace));
+        int n= word1.length(), m= word2.length();
+        int[][] dp= new int[n+1][m+1];
+        for (int j=0; j<=m; j++)
+            dp[0][j]= j;
+        for (int i=1; i<=n; i++)
+            dp[i][0]= i;
+        for (int i=1; i<=n; i++){
+            for (int j=1; j<=m; j++){
+                if (word1.charAt(i-1)==word2.charAt(j-1))
+                    dp[i][j]= dp[i-1][j-1];
+                else {
+                    dp[i][j]= Math.min(1+dp[i][j-1],Math.min(1+dp[i-1][j],1+dp[i-1][j-1]));
+                }
+            }
         }
+        return dp[n][m];
     }
-    //TC is O(m*n) and SC is O(m*n + (m+n))
+    //TC is O(n*m) and SC is O(n*m)
 }
