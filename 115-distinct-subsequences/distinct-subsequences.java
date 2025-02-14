57563cb6a51a433d3ed600958768aa5b5887b7f6
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2025-02-14 21:38:56 +0530

    Time: 19 ms (64.19%) | Memory: 51.4 MB (41.84%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,24 +1,20 @@
 class Solution {
-    //METHOD 3:- MEMOIZATION (TOP-DOWN APPROACH)
+    //METHOD 4:- TABULATION (BOTTOM-UP APPROACH)
     public int numDistinct(String s, String t) {
         int n= s.length(), m= t.length();
-        int[][] dp= new int[n][m];
-        for (int[] row: dp)
-            Arrays.fill(row,-1);
-        return memoize(s,t,n-1,m-1,dp);
+        int[][] dp= new int[n+1][m+1];
+        for (int i=0; i<=n; i++){
+            dp[i][0] = 1;
+        }
+        for (int i=1; i<=n; i++){
+            for (int j=1; j<=m; j++){
+                if (s.charAt(i-1)==t.charAt(j-1))
+                    dp[i][j]= dp[i-1][j-1] + dp[i-1][j]; //pick + not-pick
+                else
+                    dp[i][j]= dp[i-1][j]; //not-pick
+            }
+        }
+        return dp[n][m];
     }
-    //Helper Method
-    private int memoize(String s, String t, int i, int j, int[][] dp){
-        if (j<0)
-            return 1;
-        if (i<0)
-            return 0;
-        if (dp[i][j]!=-1)
-            return dp[i][j];
-        if (s.charAt(i)==t.charAt(j))
-            return dp[i][j]= memoize(s,t,i-1,j-1,dp) + memoize(s,t,i-1,j,dp); //pick + not-pick
-        else
-            return dp[i][j]= memoize(s,t,i-1,j,dp); //not-pick
-    }
-    //TC is O(n*m) and SC is O(n*m + (n+m))
+    //TC is O(n*m) and SC is O(n*m)
 }
