149a3bf2305bd4075967a8e0fe78869f1008e4cd
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2025-02-14 16:53:22 +0530

    Time: 21 ms (86.16%) | Memory: 50.5 MB (59.62%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.java
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.java
@@ -1,30 +1,35 @@
 class Solution {
-    //METHOD 1:- MEMOIZATION (TOP-DOWN APPROACH)
-    //Shifting of index concept is there (1-based indexing)
+    //METHOD 2:- TABULATION (BOTTOM-UP APPROACH)  [OPTIMAL SOLUTION]
+    //Shifting of indices is there that is 1-based indexing
     public String shortestCommonSupersequence(String str1, String str2) {
-        int n= str1.length(), m= str2.length();
-        int[][] dp= new int[n+1][m+1];
-        for (int[] row: dp)
-            Arrays.fill(row,-1);
+        int n = str1.length(), m = str2.length();
+        int[][] dp = new int[n + 1][m + 1];
 
-        //Forming the DP Table
-        memoize(str1,str2,n,m,dp);
+        //Creating DP Table
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
 
-        StringBuilder sb= new StringBuilder();
-        int i= n, j= m;
-        while (i>0 && j>0){
-            if (str1.charAt(i-1)==str2.charAt(j-1)){ //Move diagonally up-left
-                sb.append(str1.charAt(i-1));
+        int i = n, j = m;
+        StringBuilder sb = new StringBuilder();
+        while (i > 0 && j > 0) {
+            if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                sb.append(str1.charAt(i - 1));
                 i--; j--;
-            }
-            else if (dp[i-1][j]>dp[i][j-1]){ //Move up
-                sb.append(str1.charAt(i-1));
+            } else if (dp[i - 1][j] > dp[i][j - 1]) { //Up
+                sb.append(str1.charAt(i - 1));
                 i--;
-            }
-            else{ //Move up
-                sb.append(str2.charAt(j-1));
+            } else { //Left
+                sb.append(str2.charAt(j - 1));
                 j--;
             }
+
         }
         while (i>0){
             sb.append(str1.charAt(i-1));
@@ -36,15 +41,5 @@ class Solution {
         }
         return sb.reverse().toString();
     }
-    //Helper Method
-    private int memoize(String str1, String str2, int i, int j, int[][] dp){
-        if (i<= 0 || j<=0)
-            return 0;
-        if (dp[i][j]!=-1)
-            return dp[i][j];
-        if (str1.charAt(i-1)==str2.charAt(j-1))
-            return dp[i][j]= 1 + memoize(str1,str2,i-1,j-1,dp);
-        return dp[i][j]= Math.max(memoize(str1,str2,i-1,j,dp),memoize(str1,str2,i,j-1,dp));
-    }
-    //TC is O(m*n + (m+n)) and SC is O(m*n + (m+n))
+    //TC is O(m*n + (m+n)) and SC is O(m*n)
 }
