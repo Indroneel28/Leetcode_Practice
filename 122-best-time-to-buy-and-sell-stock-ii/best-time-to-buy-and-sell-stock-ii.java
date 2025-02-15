eaab2b82614b9427c6fd6a891df0606d6d890a6e
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2025-02-16 01:09:01 +0530

    Time: 2 ms (13.47%) | Memory: 46 MB (33.35%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,22 +1,22 @@
 class Solution {
-    //METHOD 3:- TABULATION (BOTTOM-UP APPROACH)
+    //METHOD 4:- SPACE OPTIMIZATION
     public int maxProfit(int[] prices) {
         int n= prices.length;
-        int[][] dp= new int[n+1][2];
-
+        int[] curr= new int[2], ahead= new int[2];
         //Base case
-        dp[n][0]= dp[n][1]= 0;
+        ahead[0]= ahead[1]= 0;
         for (int index=n-1; index>=0; index--){
             for (int buy=0; buy<=1; buy++){
                 if (buy==1){
-                    dp[index][buy]= Math.max(-prices[index]+dp[index+1][0],dp[index+1][1]);
+                    curr[buy]= Math.max(-prices[index]+ahead[0],ahead[1]);
                 }
                 else {
-                    dp[index][buy]= Math.max(prices[index]+dp[index+1][1],dp[index+1][0]);
+                    curr[buy]= Math.max(prices[index]+ahead[1],ahead[0]);
                 }
             }
+            ahead= Arrays.copyOfRange(curr,0,2);
         }
-        return dp[0][1];
+        return ahead[1];
     }
-    //TC is O(n*2) and SC is O(n*2)
+    //TC is O(n*2) and SC is O(4)
 }
