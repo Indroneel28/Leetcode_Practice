86eff5877ccdca1f2ef73005f25ef3b891e17df6
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2025-02-15 16:58:55 +0530

    Time: 15 ms (73.29%) | Memory: 49.6 MB (6.41%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -1,62 +1,43 @@
 class Solution {
-    //METHOD 2:- MEMOIZATION (TOP-DOWN APPROACH) [DONE BY NEEL OWN  WITHOUT STRIVER]
+    //METHOD 3:- MEMOIZATION (TOP-DOWN APPROACH) [STRIVER'S METHOD]
     public boolean isMatch(String s, String p) {
-        while (p.contains("**")) {
-            p = p.replace("**","*");
-        }
-        // if (p.startsWith("*"))
-        //     p= p.substring(1);
-        // if (p.isEmpty())
-        //     return true;
-        int[][] dp= new int[p.length()][s.length()];
+        int n= p.length(), m= s.length();
+        int[][] dp= new int[n][m];
         for (int[] row: dp)
             Arrays.fill(row,-1);
-        return memoize(p,s,p.length()-1,s.length()-1,dp);
+        return memoize(p,s,n-1,m-1,dp);
     }
     //Helper Method
-    private boolean memoize(String str1, String str2, int i, int j, int[][] dp){
-        //System.out.println(i+" "+j);
+    private boolean memoize(String pattern, String str, int i, int j, int[][] dp){
+        //Base cases
         if (i<0 && j<0)
             return true;
-        else if (i<0)
+        else if (i<0) //pattern pointer over
             return false;
-        else if (j<0){
-            String k= str1.substring(0,i+1);
-            for (int m=0; m<=i; m++){
-                if (k.charAt(m)!='*')
+        else if (j<0){ //string pointer over
+            //Check whether everything in remaining pattern is '*'
+            for (int k=0; k<=i; k++){
+                if (pattern.charAt(k)!='*')
                     return false;
             }
             return true;
         }
         if (dp[i][j]!=-1)
-            return dp[i][j]==1; //true if 1 else false
-        if (str1.charAt(i)==str2.charAt(j)){
-            dp[i][j]= memoize(str1,str2,i-1,j-1,dp) ? 1 : 0;
+            return dp[i][j]==1; //true or false
+        
+        if (pattern.charAt(i)==str.charAt(j) || pattern.charAt(i)=='?') {
+            dp[i][j] = memoize(pattern, str, i - 1, j - 1, dp) ? 1 : 0;
             return dp[i][j]==1;
         }
-        else if (str1.charAt(i)=='?'){
-            dp[i][j]= memoize(str1,str2,i-1,j-1,dp) ? 1 : 0;
+        else if (pattern.charAt(i)=='*'){
+            // pick OR not-pick
+            dp[i][j]= (memoize(pattern,str,i-1,j,dp) || memoize(pattern,str,i,j-1,dp)) ? 1 : 0;
             return dp[i][j]==1;
-        }
-        else if (str1.charAt(i)=='*'){
-            if(i==0) {
-                dp[i][j]= 1;
-                return true;
-            }
-            char c= str1.charAt(i-1);
-            if (c==str2.charAt(j) || c=='?'){ //matches
-                dp[i][j]= (memoize(str1,str2,i-2,j-1,dp) || memoize(str1,str2,i,j-1,dp)) ? 1 : 0;//pick || not-pick
-                return dp[i][j]==1; 
-            }
-            else {
-                dp[i][j]= memoize(str1,str2,i,j-1,dp) ? 1 : 0;
-                return dp[i][j]==1;
-            }
         }
         else {
             dp[i][j]= 0;
             return false;
         }
     }
-    //TC is O(m*n) and SC is O(m*n + (m+n))
+    //TC is O(n*m) and SC is O(n*m + (n+m))
 }
