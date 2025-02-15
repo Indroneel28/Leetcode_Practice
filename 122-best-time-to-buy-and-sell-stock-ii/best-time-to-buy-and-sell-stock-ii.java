53ae6fdc9dd7c23f5585764737cd001dbb3c86db
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2025-02-16 01:16:11 +0530

    Time: 2 ms (13.47%) | Memory: 45.8 MB (56.22%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,22 +1,20 @@
 class Solution {
-    //METHOD 4:- SPACE OPTIMIZATION
+    //METHOD 5:- USING 4 VARIABLES
     public int maxProfit(int[] prices) {
+        int currBuy, currNotBuy, aheadBuy, aheadNotBuy;
+        aheadBuy= aheadNotBuy= 0;
         int n= prices.length;
-        int[] curr= new int[2], ahead= new int[2];
-        //Base case
-        ahead[0]= ahead[1]= 0;
-        for (int index=n-1; index>=0; index--){
-            for (int buy=0; buy<=1; buy++){
-                if (buy==1){
-                    curr[buy]= Math.max(-prices[index]+ahead[0],ahead[1]);
-                }
-                else {
-                    curr[buy]= Math.max(prices[index]+ahead[1],ahead[0]);
-                }
-            }
-            ahead= Arrays.copyOfRange(curr,0,2);
+        for (int index= n-1; index>=0; index--){
+            //Buying
+            currBuy= Math.max(-prices[index]+aheadNotBuy,aheadBuy);
+            
+            //Selling (Not Buying)
+            currNotBuy= Math.max(prices[index]+aheadBuy,aheadNotBuy);
+            
+            aheadBuy= currBuy;
+            aheadNotBuy= currNotBuy;
         }
-        return ahead[1];
+        return aheadBuy;
+        //TC is O(n) and SC is O(4)
     }
-    //TC is O(n*2) and SC is O(4)
 }
