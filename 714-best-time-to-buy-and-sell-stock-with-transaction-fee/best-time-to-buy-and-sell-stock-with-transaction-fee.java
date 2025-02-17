3397487c416e6bca972ba5d306a002859298946a
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2025-02-18 01:44:01 +0530

    Time: 4 ms (99.50%) | Memory: 54.5 MB (99.43%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,20 +1,19 @@
 class Solution {
-    //METHOD 3:- SPACE OPTIMIZATION
+    //METHOD 4:- USING 4 VARIABLES [OPTIMAL METHOD]
     public int maxProfit(int[] prices, int fee) {
         int n= prices.length;
-        int[] curr= new int[2], ahead= new int[2];
+        int currBuy, aheadBuy= 0, currNotBuy, aheadNotBuy= 0;
         for (int index=n-1; index>=0; index--){
-            for (int buy=0; buy<=1; buy++){
-                if (buy==1){ //Buy
-                    curr[buy]= Math.max(-prices[index]+ahead[0],ahead[1]);
-                }
-                else { //Sell
-                    curr[buy]= Math.max(prices[index]-fee+ahead[1],ahead[0]); //cut fees if selling
-                }
-            }
-            ahead= Arrays.copyOfRange(curr,0,2);
+            //Buying
+            currBuy=  Math.max(-prices[index]+aheadNotBuy,aheadBuy);
+            
+            //Selling
+            currNotBuy= Math.max(prices[index]-fee+aheadBuy,aheadNotBuy);
+            
+            aheadBuy= currBuy;
+            aheadNotBuy= currNotBuy;
         }
-        return ahead[1];
+        return aheadBuy;
     }
-    //TC is O(n*2) and SC is O(2*2)
+    //TC is O(n) and SC is O(4)
 }
