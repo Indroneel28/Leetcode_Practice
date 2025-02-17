55f92033ac35ec25ddddc90c55ec206139e4959a
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2025-02-17 16:51:51 +0530

    Time: 21 ms (46.03%) | Memory: 58.5 MB (94.34%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,25 +1,20 @@
 class Solution {
-    //Instead of three parameters like earlier :- index, buy, again
-    //METHOD 5:- MEMOIZATION (STRIVER'S METHOD) USING TWO PARAMETERS:- index, transaction
+    //METHOD 6:- TABULATION with 2D DP
     public int maxProfit(int[] prices) {
-        int[][] dp= new int[prices.length][2*2]; // buy * days
-        for (int[] row: dp)
-            Arrays.fill(row,-1);
-        return memoize(prices,0,0,dp);
-    }
-    //Helper Method
-    private int memoize(int[] prices, int index, int transaction, int[][] dp){
-        if (index==prices.length || transaction==4)
-            return 0;
-        if (dp[index][transaction]!=-1)
-            return dp[index][transaction];
-
-        if (transaction%2==0){ //Buy
-            return dp[index][transaction]= Math.max(-prices[index]+memoize(prices,index+1,transaction+1,dp),memoize(prices,index+1,transaction,dp));
+        int n= prices.length;
+        int[][] dp= new int[n+1][5];
+        for (int index=n-1; index>=0; index--){
+            for (int transaction=0; transaction<4; transaction++){
+                if (transaction%2==0){
+                    dp[index][transaction]= Math.max(-prices[index]+dp[index+1][transaction+1],dp[index+1][transaction]);
+                }
+                else{
+                    dp[index][transaction]= Math.max(prices[index]+dp[index+1][transaction+1],dp[index+1][transaction]);
+                }
+            }
         }
-        else{ //Sell
-            return dp[index][transaction]= Math.max(prices[index]+memoize(prices,index+1,transaction+1,dp),memoize(prices,index+1,transaction,dp));
-        }
+        //System.out.println(Arrays.deepToString(dp));
+        return dp[0][0];
     }
-    //TC is O(n*4) and SC is O(n*4 + n)
+    //TC is O(n*4) and SC is O(n*4)
 }
