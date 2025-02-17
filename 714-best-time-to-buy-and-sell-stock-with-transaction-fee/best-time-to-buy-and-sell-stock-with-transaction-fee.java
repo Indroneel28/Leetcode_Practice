67f0d431803c89f1614092ccd1ab8594deec9050
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2025-02-18 01:25:05 +0530

    Time: 19 ms (38.95%) | Memory: 55.3 MB (55.19%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,24 +1,19 @@
 class Solution {
-    //METHOD 1:- MEMOIZATION (TOP-DOWN APPROACH)
+    //METHOD 2:- TABULATION (BOTTOM-UP APPROACH)
     public int maxProfit(int[] prices, int fee) {
-        int[][] dp= new int[prices.length][2];
-        for (int[] arr: dp)
-            Arrays.fill(arr,-1);
-        return memoize(prices,0,1,dp,fee);
-    }
-    //Helper Method
-    private int memoize(int[] prices, int index, int buy, int[][] dp, int fee){
-        if (index==prices.length)
-            return 0;
-        if (dp[index][buy]!=-1)
-            return dp[index][buy];
-        
-        if (buy==1){ //Buy
-            return dp[index][buy]= Math.max(-prices[index]+memoize(prices,index+1,0,dp,fee),memoize(prices,index+1,1,dp,fee));
+        int n= prices.length;
+        int[][] dp= new int[n+1][2];
+        for (int index=n-1; index>=0; index--){
+            for (int buy=0; buy<=1; buy++){
+                if (buy==1){ //Buy
+                    dp[index][buy]= Math.max(-prices[index]+dp[index+1][0],dp[index+1][1]);
+                }
+                else { //Sell
+                    dp[index][buy]= Math.max(prices[index]-fee+dp[index+1][1],dp[index+1][0]); //cut fees if selling
+                }
+            }
         }
-        else {
-            return dp[index][buy]= Math.max(prices[index]-fee+memoize(prices,index+1,1,dp,fee),memoize(prices,index+1,0,dp,fee));
-        }
+        return dp[0][1];
     }
-    //TC is O(n*2) and SC is O(n*2 + n)
+    //TC is O(n*2) and SC is O(n*2)
 }
