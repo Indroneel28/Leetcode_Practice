5f058359a96949320c98d8fb9a57b6452f6ab680
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2025-02-18 01:35:55 +0530

    Time: 34 ms (18.16%) | Memory: 55.7 MB (36.38%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,19 +1,20 @@
 class Solution {
-    //METHOD 2:- TABULATION (BOTTOM-UP APPROACH)
+    //METHOD 3:- SPACE OPTIMIZATION
     public int maxProfit(int[] prices, int fee) {
         int n= prices.length;
-        int[][] dp= new int[n+1][2];
+        int[] curr= new int[2], ahead= new int[2];
         for (int index=n-1; index>=0; index--){
             for (int buy=0; buy<=1; buy++){
                 if (buy==1){ //Buy
-                    dp[index][buy]= Math.max(-prices[index]+dp[index+1][0],dp[index+1][1]);
+                    curr[buy]= Math.max(-prices[index]+ahead[0],ahead[1]);
                 }
                 else { //Sell
-                    dp[index][buy]= Math.max(prices[index]-fee+dp[index+1][1],dp[index+1][0]); //cut fees if selling
+                    curr[buy]= Math.max(prices[index]-fee+ahead[1],ahead[0]); //cut fees if selling
                 }
             }
+            ahead= Arrays.copyOfRange(curr,0,2);
         }
-        return dp[0][1];
+        return ahead[1];
     }
-    //TC is O(n*2) and SC is O(n*2)
+    //TC is O(n*2) and SC is O(2*2)
 }
