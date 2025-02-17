1a24aa4f7e02a982d29fe080975b32febfa920de
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2025-02-17 16:37:26 +0530

    Time: 131 ms (16.36%) | Memory: 76.5 MB (21.53%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,25 +1,25 @@
 class Solution {
-    //METHOD 4:- TABULATION (BOTTOM-UP APPROACH)
+    //Instead of three parameters like earlier :- index, buy, again
+    //METHOD 5:- MEMOIZATION (STRIVER'S METHOD) USING TWO PARAMETERS:- index, transaction
     public int maxProfit(int[] prices) {
-        int n= prices.length;
-        int[][][] dp= new int[n+1][2][3]; // n - buy - again
-
-        //no need of base case as it initializes to 0 which is ofcourse
-
-        for (int index=n-1; index>=0; index--){
-            for (int buy= 0; buy<=1; buy++){
-                for (int again= 0; again<=1; again++){
-                    if (buy==1) { //Allowed to buy
-                        dp[index][buy][again] = Math.max(-prices[index]+dp[index+1][0][again],dp[index+1][1][again]);
-                    }
-                    else { //Not allowed to buy -> Sell
-                        dp[index][buy][again] = Math.max(prices[index]+dp[index+1][1][again+1],dp[index+1][0][again]);
-                    }
-                }
-            }
-        }
-        //System.out.println(Arrays.deepToString(dp));
-        return dp[0][1][0];
+        int[][] dp= new int[prices.length][2*2]; // buy * days
+        for (int[] row: dp)
+            Arrays.fill(row,-1);
+        return memoize(prices,0,0,dp);
     }
-    //TC is O(n*2*3) and SC is O(n*2*3)
+    //Helper Method
+    private int memoize(int[] prices, int index, int transaction, int[][] dp){
+        if (index==prices.length || transaction==4)
+            return 0;
+        if (dp[index][transaction]!=-1)
+            return dp[index][transaction];
+
+        if (transaction%2==0){ //Buy
+            return dp[index][transaction]= Math.max(-prices[index]+memoize(prices,index+1,transaction+1,dp),memoize(prices,index+1,transaction,dp));
+        }
+        else{ //Sell
+            return dp[index][transaction]= Math.max(prices[index]+memoize(prices,index+1,transaction+1,dp),memoize(prices,index+1,transaction,dp));
+        }
+    }
+    //TC is O(n*4) and SC is O(n*4 + n)
 }
