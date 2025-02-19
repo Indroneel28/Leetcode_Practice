31838955e8eb5d37a9cd7be78edd76ab138e5819
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2025-02-19 23:24:50 +0530

    Time: 21 ms (32.80%) | Memory: 45.3 MB (28.47%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -2,14 +2,14 @@ class Solution {
     //USING RECURSION AND BACKTRACKING
     public String getHappyString(int n, int k) {
         ArrayList<String> list = new ArrayList<>();
-        traverse(n, list, new boolean[n], "");
+        traverse(n, list, "");
         if (list.size()<k)
             return "";
         return list.get(k-1);
     }
 
     //Helper Method
-    private void traverse(int n, ArrayList<String> list, boolean[] check, String s) {
+    private void traverse(int n, ArrayList<String> list, String s) {
         if (s.length() == n) {
             //System.out.println(s);
             list.add(s);
@@ -18,7 +18,7 @@ class Solution {
 
         for (char c = 'a'; c <= 'c'; c++) {
             if (s.isEmpty() || s.charAt(s.length() - 1) != c) {
-                traverse(n, list, check, s + c);
+                traverse(n, list, s + c);
             }
         }
 
