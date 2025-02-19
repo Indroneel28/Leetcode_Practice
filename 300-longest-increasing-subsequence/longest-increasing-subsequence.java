2d6c313c951f2bbf7bd68e2ff177b699be206c7f
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2025-02-19 14:04:15 +0530

    Time: 74 ms (24.52%) | Memory: 45.2 MB (22.60%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,22 +1,22 @@
 class Solution {
-    //METHOD 3:- TABULATION (BOTTOM-UP APPROACH)
+    //METHOD 4:- SPACE OPTIMIZATION
     public int lengthOfLIS(int[] nums) {
         int n= nums.length;
-        int[][] dp= new int[n+1][n+1];
+        int[] curr= new int[n+1], ahead= new int[n+1];
         for (int index=n-1; index>=0; index--){
             //Shifting of indices for prevIndex
             for (int prevIndex= index-1; prevIndex>=-1; prevIndex--){
                 //not-pick
-                int notPick= dp[index+1][prevIndex+1];
+                int notPick= ahead[prevIndex+1];
                 int pick= 0;
                 if (prevIndex==-1 || nums[index]>nums[prevIndex]){
-                    pick= 1 + dp[index+1][index+1];
+                    pick= 1 + ahead[index+1];
                 }
-                dp[index][prevIndex+1]= Math.max(pick,notPick);
+                curr[prevIndex+1]= Math.max(pick,notPick);
             }
+            ahead= Arrays.copyOfRange(curr,0,n+2);
         }
-        //System.out.println(Arrays.deepToString(dp));
-        return dp[0][-1+1];
+        return ahead[-1+1];
     }
-    //TC is O(n^2) and SC is O(n^2)
+    //TC is O(n^2) and SC is O(2n)
 }
