bb620ff22c66e042c8327e1ff8259823aa4d086b
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2025-03-04 00:16:31 +0530

    Time: 4 ms (100.00%) | Memory: 68.4 MB (10.03%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,22 +1,23 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        //METHOD 1:- USING ARRAYLIST
-        ArrayList<Integer> list1= new ArrayList<>();
-        ArrayList<Integer> list2= new ArrayList<>();
-        for (int num: nums){
-            if (num<pivot)
-                list1.add(num);
-            else if (num==pivot)
-                list2.add(0,num);
-            else 
-                list2.add(num);;
+        //METHOD 2:- USING TWO POINTERS AND ARRAY
+        int left= 0, right= nums.length-1;
+        int[] temp= new int[nums.length];
+
+        for(int i=0; i<nums.length; i++){
+            if (nums[i]<pivot)
+                temp[left++]= nums[i];
         }
-        int k=0;
-        for (int ele: list1)
-            nums[k++]= ele;
-        for (int ele: list2)
-            nums[k++]= ele;
-        return nums;
+
+        for (int j=nums.length-1; j>=0; j--){
+            if (nums[j]>pivot)
+                temp[right--]= nums[j];
+        }
+
+        for (int i=left; i<=right; i++){
+            temp[i]= pivot;
+        }
+        return temp;
         //TC is O(2n) and SC is O(n)
     }
 }
