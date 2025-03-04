0a946da5bf02600b1834ac8b2f34a01edece502f
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2025-03-04 23:57:17 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (43.90%) - LeetSync

---
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.java
@@ -0,0 +1,19 @@
+class Solution {
+    //METHOD 1:- BRUTE FORCE METHOD
+    public boolean checkPowersOfThree(int n) {
+        int power= 0;
+        while (Math.pow(3,power)<=n){
+            power++;
+        }
+
+        power--;
+        while (n>0){
+            if (n>=Math.pow(3,power))
+                n= n-(int)Math.pow(3,power);
+            if (n>= Math.pow(3,power))
+                return false;
+            power--;
+        }
+        return true;
+    }
+}
