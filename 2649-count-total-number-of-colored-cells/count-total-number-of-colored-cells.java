afcc8b261a31786cdcbcaa14ecc5887ea6f305f4
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2025-03-05 18:18:42 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (59.02%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.java
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.java
@@ -1,11 +1,16 @@
 class Solution {
     public long coloredCells(int n) {
         //METHOD 1:- MATHEMATICAL LOGIC AND CALCULATION
+        /*
         long ans= 1;
         for(int i=1; i<n; i++){
             ans += (4*i);
         }
         return ans;
+        */
         //TC is O(n) and SC is O(1)
+
+        //METHOD 2:- USING FORMULA (LESS INTUITIVE)
+        return 2*((long)n-1)*(long)n + 1;
     }
 }
