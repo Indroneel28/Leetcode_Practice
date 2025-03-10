5a1564eda84a181d0cdcfacc4646a07cf7b32e5a
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2025-03-11 01:56:49 +0530

    Time: 6 ms (81.75%) | Memory: 54.7 MB (92.48%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
@@ -6,12 +6,10 @@ class Solution {
         for (int i=0; i<matrix.length; i++){
             dp[i][0]= matrix[i][0];
             countSquareSubmatrices += dp[i][0];
-            System.out.println(countSquareSubmatrices);
         }
         for (int j=1; j<matrix[0].length; j++){
             dp[0][j]= matrix[0][j];
             countSquareSubmatrices += dp[0][j];
-            System.out.println(countSquareSubmatrices);
         }
 
         for (int i=1; i<matrix.length; i++){
@@ -19,11 +17,9 @@ class Solution {
                 if (matrix[i][j]==1) {
                     dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
                     countSquareSubmatrices += dp[i][j];
-                    System.out.println(countSquareSubmatrices);
                 }
             }
         }
-        System.out.println(Arrays.deepToString(dp));
         return countSquareSubmatrices;
     }
     //TC is O(n*m) and SC is O(n*m)
