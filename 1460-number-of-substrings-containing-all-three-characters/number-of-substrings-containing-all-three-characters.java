d148e878d47341a7bb70bd9b45e8a4842c80e025
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2025-03-11 23:31:42 +0530

    Time: 11 ms (91.15%) | Memory: 44.7 MB (49.66%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -1,17 +1,15 @@
 class Solution {
+    //METHOD 2:- USING SLIDING WINDOW
     public int numberOfSubstrings(String s) {
-        //OPTIMAL METHOD:- USING SLIDING WINDOW
-        int[] lastseen= {-1,-1,-1}; //last seen for a, and c
-        int substrings= 0;
+        int count= 0;
+        int[] store= {-1,-1,-1};
         for (int i=0; i<s.length(); i++){
-            char c= s.charAt(i);
-            lastseen[c-97]= i;
-            
-            int min= Math.min(lastseen[0],Math.min(lastseen[1],lastseen[2]));
-            if (min != -1)
-                substrings += min+1;
+            store[s.charAt(i)-'a']= i;
+            int mini= Math.min(store[0],Math.min(store[1],store[2]));
+            if (mini!=-1)
+                count += (mini+1);
         }
-        return substrings;
-        //TC is O(n) and SC is O(3)
+        return count;
     }
+    //TC is O(n) and SC is O(3)
 }
