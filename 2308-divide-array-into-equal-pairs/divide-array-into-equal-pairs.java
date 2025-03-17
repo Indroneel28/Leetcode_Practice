a3846a341076bec40399eddd8653bd64be3fdcfb
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2025-03-17 20:22:41 +0530

    Time: 8 ms (40.30%) | Memory: 44.6 MB (39.07%) - LeetSync

--- 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.java
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.java
@@ -1,27 +1,16 @@
 class Solution {
     public boolean divideArray(int[] nums) {
-        //METHOD 1:- USING HASHMAP
-        /*
-        HashMap<Integer,Integer> hashMap= new HashMap<>();
-        for (int num: nums){
-            hashMap.put(num,hashMap.getOrDefault(num,0)+1);
+        //USING HASHMAP
+        HashMap<Integer,Integer> map= new HashMap<>();
+        for(int num: nums){
+            map.put(num,map.getOrDefault(num,0)+1);
         }
-        for (int ele: hashMap.keySet()){
-            if (hashMap.get(ele)%2!=0)
-                return false;
-        }
-        return true;
-         */
 
-        //METHOD 2:- USING ARRAY
-        //1 <= nums[i] <= 500
-        int[] store= new int[500+1];
-        for (int num: nums)
-            store[num]++;
-        for (int ele: store){
-            if (ele%2==1)
+        for(int val: map.values()){
+            if(val%2==1)
                 return false;
         }
         return true;
+        //TC is O(2n) and SC is O(2n)
     }
 }
