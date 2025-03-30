170fe10058e74c0ba53d92f9e95b751b6aeae4da
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2025-03-30 19:36:44 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (27.50%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -14,24 +14,32 @@
  * }
  */
 class Solution {
-    //METHOD 2:- ITERATIVE SOLUTION using Stack
+    //METHOD 3:- Using Morris Traversal Algorithm
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> list= new ArrayList<>();
-        if (root==null)
-            return list;
-        Stack<TreeNode> stack= new Stack<>();
+        List<Integer> answer= new ArrayList<>();
         TreeNode curr= root;
-
-        while (curr!=null || !stack.isEmpty()){
-            while (curr!=null){
-                stack.push(curr);
-                curr= curr.left;
+        while (curr!=null){
+            if (curr.left==null){
+                answer.add(curr.val);
+                curr= curr.right;
             }
-            curr= stack.pop();
-            list.add(curr.val);
-            curr= curr.right;
+            else{
+                TreeNode prev= curr.left;
+                while (prev.right!=null && prev.right!=curr){
+                    prev= prev.right;
+                }
+                if (prev.right==null){
+                    prev.right= curr;
+                    curr= curr.left;
+                }
+                else {
+                    prev.right= null;
+                    answer.add(curr.val);
+                    curr= curr.right;
+                }
+            }
         }
-        return list;
+        return answer;
     }
-    //TC is O(n) and SC is O(n)
+    //TC is O(n) and SC is O(1)
 }
