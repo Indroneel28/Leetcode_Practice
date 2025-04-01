6a43eea9019fe49ab3e006d9cf894c3d8a62efe4
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2025-04-01 19:35:07 +0530

    Time: 0 ms (100.00%) | Memory: 45.5 MB (99.59%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -14,35 +14,49 @@
  * }
  */
 class Solution {
-    //METHOD 1:- RECURSIVE METHOD
+    //METHOD 2:- ITERATIVE METHOD
+    //Used by Striver
     public TreeNode deleteNode(TreeNode root, int key) {
-        if(root==null)
+        
+        //Edge cases
+        if (root==null)
             return null;
-        else if (root.val<key) //Move right
-            root.right= deleteNode(root.right,key);
-        else if (key<root.val)//Move left
-            root.left= deleteNode(root.left,key);
-        else {
-            //root needs to be deleted
-            //Case 1:- No child
-            if (root.left==null && root.right==null)
-                return null;
-            else if (root.left==null)
-                return root.right;
-            else if (root.right==null)
-                return root.left;
-            else {
-                TreeNode newNode= findMin(root.right);
-                root.val= newNode.val;
-                root.right= deleteNode(root.right, newNode.val);
+        if (root.val==key)
+            return helper(root);
+        
+        TreeNode p= root;
+        while (p!=null){
+            if (p.val>key){
+                if (p.left!=null && p.left.val==key)
+                    p.left= helper(p.left);
+                else
+                    p= p.left;
+            }
+            else{
+                if (p.right!=null && p.right.val==key)
+                    p.right= helper(p.right);
+                else
+                    p= p.right;
             }
         }
         return root;
     }
-    private TreeNode findMin(TreeNode root){
-        while (root.left!=null)
-            root= root.left;
-        return root;
+    private TreeNode helper(TreeNode root){
+        if (root.left==null)
+            return root.right;
+        else if (root.right==null)
+            return root.left;
+        else {
+            TreeNode r= root.right;
+            TreeNode last= findLastRight(root.left);
+            last.right= r;
+            return root.left;
+        }
     }
-    //TC is O(logn) and SC is O(logn)
+    private TreeNode findLastRight(TreeNode node){
+        while (node.right!=null)
+            node= node.right;
+        return node;
+    }
+    //TC is O(logn) and SC is O(1)
 }
