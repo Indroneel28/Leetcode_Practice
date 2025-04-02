5e29e43484d1ca6f590fb2b4579c046c6c819984
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2025-04-02 23:40:02 +0530

    Time: 5 ms (100.00%) | Memory: 45.4 MB (16.84%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -9,25 +9,14 @@
  */
 
 class Solution {
-    //METHOD 1:- CONCEPT OF LCA OF BT
+    //METHOD 2:- LCA of BST
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        return traverse(root,p,q);
-    }
-    private TreeNode traverse(TreeNode root, TreeNode p, TreeNode q){
-        if (root==null)
-            return null;
-        TreeNode l= traverse(root.left,p,q);
-        TreeNode r= traverse(root.right,p,q);
-        if (l!=null && r!=null)
+        if (p.val < root.val && q.val<root.val)
+            return lowestCommonAncestor(root.left,p,q);
+        else if (p.val > root.val && q.val > root.val)
+            return lowestCommonAncestor(root.right,p,q);
+        else
             return root;
-        else if (root==p || root==q)
-            return root;
-        else if (l!=null)
-            return l;
-        else if (r!=null)
-            return r;
-        else //both are null
-            return null;
     }
-    //TC is O(n) and SC is O(n)
+    //TC is O(logn) and SC is O(logn)
 }
