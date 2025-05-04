530f81751998399d87b60613f481b348c0e3315e
Indroneel Goswami <122139794+Indroneel28@users.noreply.github.com>
2025-05-04 23:00:25 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (55.08%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
@@ -14,30 +14,35 @@
  * }
  */
 class Solution {
-    //METHOD 1:- CONSTRUCTING THE BST
+    //METHOD 1:- Brute Force Method
     public TreeNode bstFromPreorder(int[] preorder) {
         TreeNode root= new TreeNode(preorder[0]);
         for (int i=1; i<preorder.length; i++){
-            TreeNode node= new TreeNode(preorder[i]);
-            TreeNode p= root;
-            while (p!=null){
-                if (node.val<p.val){
-                    if (p.left==null) {
-                        p.left = node;
-                        break;
-                    }
-                    p= p.left;
-                }
-                else {
-                    if (p.right==null){
-                        p.right= node;
-                        break;
-                    }
-                    p= p.right;
-                }
-            }
+            insertNode(root,preorder[i]);;
         }
         return root;
     }
-    //TC is O(nlogn) and SC is O(1)
+    private void insertNode(TreeNode root, int value){
+        TreeNode node= new TreeNode(value);
+        TreeNode temp= root;
+        while (temp!=null){
+            if (temp.val>value){
+                if (temp.left==null){
+                    temp.left= node;
+                    return;
+                }
+                else 
+                    temp = temp.left;
+            }
+            else{
+                if (temp.right==null){
+                    temp.right= node;
+                    return;
+                }
+                else 
+                    temp = temp.right;
+            }
+        }
+    }
+    //TC is O(n^2) and SC is O(1)
 }
